2e35f1782e155e18ccadea4097b3cf26fffe9bbc
alicepyne <alice.pyne@ucl.ac.uk>
2019-02-19 14:35:46 +0000

    Updated to remove plotting functionality as this is now in statsplotting
    Signed-off-by: alicepyne <alice.pyne@ucl.ac.uk>

--- pygwytracing.py
+++ pygwytracing.py
@@ -10,6 +10,11 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from math import sqrt,acos, degrees, atan2, pi, cos, sin
+from scipy import spatial, interpolate as interp
+from scipy.ndimage import morphology as morph, measurements, fourier, interpolation as imginterp
+from skimage import feature, filters, morphology, segmentation, measure, transform
+
 # Import height thresholding.py for processing bilayer removal images
 
 ### Set seaborn to override matplotlib for plot output
@@ -105,19 +110,19 @@ def editfile(data, minheightscale, maxheightscale):
     # select each channel of the file in turn
     # this is run within the for k in chosen_ids loop so k refers to the index of each chosen channel to analyse
     gwy.gwy_app_data_browser_select_data_field(data, k)
-    # flatten the data
-    gwy.gwy_process_func_run('flatten_base', data, gwy.RUN_IMMEDIATE)
-    # level the data
-    gwy.gwy_process_func_run("level", data, gwy.RUN_IMMEDIATE)
     # align the rows
     gwy.gwy_process_func_run("align_rows", data, gwy.RUN_IMMEDIATE)  # NONINTERACTIVE is only for file modules
+    # level the data
+    gwy.gwy_process_func_run("level", data, gwy.RUN_IMMEDIATE)
+    # flatten the data
+    gwy.gwy_process_func_run('flatten_base', data, gwy.RUN_IMMEDIATE)
     # Fix zero
     gwy.gwy_process_func_run('zero_mean', data, gwy.RUN_IMMEDIATE)
-    # remove scars
-    gwy.gwy_process_func_run('scars_remove', data, gwy.RUN_IMMEDIATE)
+    # # remove scars
+    # gwy.gwy_process_func_run('scars_remove', data, gwy.RUN_IMMEDIATE)
     # Apply a 1.5 pixel gaussian filter
     data_field = gwy.gwy_app_data_browser_get_current(gwy.APP_DATA_FIELD)
-    data_field.filter_gaussian(1)
+    data_field.filter_gaussian(1.5)
     # # Shift contrast - equivalent to 'fix zero'
     # datafield.add(-data_field.get_min())
 
@@ -263,210 +268,6 @@ def grainanalysis(appended_data, filename, datafield, grains):
     grainstatsarguments = sorted(grainstatsarguments)
 
     return grainstatsarguments, grainstats, appended_data
-
-
-def plotall(dataframe, directory, extension):
-    print 'Plotting graphs for all dataframe variables in %s' % directory
-
-    # Create a saving name format/directory
-    savedir = os.path.join(directory, 'Plots')
-    savename = os.path.join(savedir, os.path.splitext(os.path.basename(directory))[0])
-    if not os.path.exists(savedir):
-        os.makedirs(savedir)
-
-    df = dataframe
-    # Select columns of datatype 'float64' i.e. not integers or strings
-    df = df.select_dtypes(include=['float64'])
-    # Iterate through all columns to plot data using seaborn
-    for i, col in enumerate(df.columns):
-        plt.figure(i)
-        sns.distplot(df[col], bins=bins)
-        # plt.hist(df[col])
-        # df[col].plot.hist()
-        plt.show()
-        plt.savefig(savename + '_' + str(col) + extension)
-
-
-def plotting(dataframe, arg1, grouparg, bins, directory, extension):
-    print 'Plotting graph of %s' % (arg1)
-    # Create a saving name format/directory
-    savedir = os.path.join(directory, 'Plots')
-    savename = os.path.join(savedir, os.path.splitext(os.path.basename(directory))[0])
-    if not os.path.exists(savedir):
-        os.makedirs(savedir)
-    df = dataframe
-
-    # Change from m to nm units for plotting
-    df[arg1] = df[arg1] * 1e9
-
-    # Generating min and max axes based on datasets
-    min_ax = df[arg1].min()
-    min_ax = round(min_ax, 9)
-    max_ax = df[arg1].max()
-    max_ax = round(max_ax, 9)
-
-    # Plot using MatPlotLib separated by the grouparg on two separate graphs with stacking
-    # Create a figure of given size
-    fig = plt.figure(figsize=(18, 12))
-    ax = fig.add_subplot(111)
-    # Set title
-    ttl = 'Histogram of %s' % arg1
-    # Pivot dataframe to get required variables in correct format for plotting
-    df1 = df.pivot(columns=grouparg, values=arg1)
-    # Plot histogram
-    df1.plot.hist(ax=ax, legend=True, bins=bins, range=(min_ax, max_ax), alpha=.3, stacked=True)
-    # Set x axis label
-    plt.xlabel('%s (nm)' % arg1)
-    # Set tight borders
-    plt.tight_layout()
-    # Set legend options
-    # plt.legend(ncol=2, loc='upper right')
-    # Save plot
-    plt.savefig(savename + '_' + arg1 + '_a' + extension)
-
-    # Plot each argument together using MatPlotLib
-    # Create a figure of given size
-    fig = plt.figure(figsize=(18, 12))
-    ax = fig.add_subplot(111)
-    # Set title
-    ttl = 'Histogram of %s' % arg1
-    # Melt dataframe to leave only columns we are interested in
-    df3 = pd.melt(df, id_vars=[arg1])
-    # Plot histogram
-    df3.plot.hist(ax=ax, bins=bins, range=(min_ax, max_ax), alpha=.3)
-    plt.xlabel('%s (nm)' % arg1)
-    # # Set legend options
-    # plt.legend(ncol=2, loc='upper right')
-    # Set tight borders
-    plt.tight_layout()
-    # Save plot
-    plt.savefig(savename + '_' + arg1 + '_b' + extension)
-
-
-def seaplotting(df, arg1, arg2, bins, directory, extension):
-    # Create a saving name format/directory
-    savedir = os.path.join(directory, 'Plots')
-    savename = os.path.join(savedir, os.path.splitext(os.path.basename(directory))[0])
-    if not os.path.exists(savedir):
-        os.makedirs(savedir)
-
-    # Change from m to nm units for plotting
-    df[arg1] = df[arg1] * 1e9
-    df[arg2] = df[arg2] * 1e9
-
-    # Generating min and max axes based on datasets
-    min_ax = min(df[arg1].min(), df[arg2].min())
-    min_ax = round(min_ax, 9)
-    max_ax = max(df[arg1].max(), df[arg2].max())
-    max_ax = round(max_ax, 9)
-
-    # Plot data using seaborn
-    with sns.axes_style('white'):
-        # sns.jointplot(arg1, arg2, data=df, kind='hex')
-        sns.jointplot(arg1, arg2, data=df, kind='reg')
-        plt.savefig(savename + '_' + str(arg1) + str(arg2) + '_seaborn' + extension)
-
-def plottingallstats(grainstatsarguments, df, extension, directory):
-
-    # Create a saving name format/directory
-    savedir = os.path.join(directory, 'Plots')
-    savename = os.path.join(savedir, os.path.splitext(os.path.basename(directory))[0])
-    if not os.path.exists(savedir):
-        os.makedirs(savedir)
-
-    for key in grainstatsarguments:
-        print 'Plotting graph of %s' % (key)
-        # Plot each argument together using MatPlotLib
-        # Create a figure of given size
-        fig = plt.figure(figsize=(18, 12))
-        ax = fig.add_subplot(111)
-        # Set title
-        ttl = 'Histogram of %s' % key
-        # Melt dataframe to leave only columns we are interested in
-        df3 = pd.melt(df, id_vars=[key])
-        # Plot histogram
-        df3.plot.hist(ax=ax, alpha=.3)
-        plt.xlabel('%s (nm)' % key)
-        # # Set legend options
-        # plt.legend(ncol=2, loc='upper right')
-        # Set tight borders
-        plt.tight_layout()
-        # Save plot
-        plt.savefig(savename + '_' + key + extension)
-
-
-
-def plotting2(df, arg1, arg2, grouparg, bins, directory, extension):
-    print 'Plotting graph of %s and %s' % (arg1, arg2)
-    # Create a saving name format/directory
-    savedir = os.path.join(directory, 'Plots')
-    savename = os.path.join(savedir, os.path.splitext(os.path.basename(directory))[0])
-    if not os.path.exists(savedir):
-        os.makedirs(savedir)
-
-    # Change from m to nm units for plotting
-    df[arg1] = df[arg1] * 1e9
-    df[arg2] = df[arg2] * 1e9
-
-    # Generating min and max axes based on datasets
-    min_ax = min(df[arg1].min(), df[arg2].min())
-    min_ax = round(min_ax, 9)
-    max_ax = max(df[arg1].max(), df[arg2].max())
-    max_ax = round(max_ax, 9)
-
-    # Plot data
-
-    # Plot each type using MatPlotLib separated by filetype on two separate graphs with stacking
-    # Create a figure of given size
-    fig = plt.figure(figsize=(28, 8))
-    # First dataframe
-    # Add a subplot to lot both sets on the same figure
-    ax = fig.add_subplot(121)
-    # Set title
-    ttl = 'Histogram of %s and %s' % (arg1, arg2)
-    # Pivot dataframe to get required variables in correct format for plotting
-    df1 = df.pivot(columns=grouparg, values=arg1)
-    # Plot histogram
-    df1.plot.hist(legend=True, ax=ax, bins=bins, range=(min_ax, max_ax), alpha=.3, stacked=True)
-    # Set x axis label
-    plt.xlabel('%s (nm)' % arg1)
-    # Set tight borders
-    plt.tight_layout()
-    # Set legend options
-    # plt.legend(ncol=2, loc='upper right')
-    # Second dataframe
-    # Add a subplot
-    ax = fig.add_subplot(122)
-    # Pivot second dataframe to get required variables in correct format for plotting
-    df2 = df.pivot(columns=grouparg, values=arg2)
-    # Plot histogram
-    df2.plot.hist(legend=True, ax=ax, bins=bins, range=(min_ax, max_ax), alpha=.3, stacked=True)
-    # Set x axis label
-    plt.xlabel('%s (nm)' % arg2)
-    # Set tight borders
-    plt.tight_layout()
-    # Set legend options
-    # plt.legend(ncol=2, loc='upper right')
-    # Save plot
-    plt.savefig(savename + '_' + arg1 + '_' + arg2 + '_' + 'a' + extension)
-
-    # Create a figure of given size
-    fig = plt.figure(figsize=(18, 12))
-    # Add a subplot
-    ax = fig.add_subplot(111)
-    # Set title
-    ttl = 'Histogram of %s and %s' % (arg1, arg2)
-    # Plot each argument together using MatPlotLib
-    df3 = pd.melt(df, id_vars=[arg1, arg2])
-    df3.plot.hist(legend=True, ax=ax, bins=bins, range=(min_ax, max_ax), alpha=.3)
-    # plt.xlabel('%s %s (nm)' % (arg1, arg2))
-    plt.xlabel('nm')
-    # # Set legend options
-    # plt.legend(ncol=2, loc='upper right')
-    # Set tight borders
-    plt.tight_layout()
-    # Save plot
-    plt.savefig(savename + '_' + arg1 + '_' + arg2 + '_' + 'b' + extension)
 
 
 def find_median_pixel_area(datafield, grains):
@@ -675,8 +476,9 @@ if __name__ == '__main__':
     # Set the file path, i.e. the directory where the files are here'
     # path = '/Users/alice/Dropbox/UCL/DNA MiniCircles/Minicircle Data Edited/New Images/Nickel_Kavit'
     # path = '/Users/alice/Dropbox/UCL/DNA MiniCircles/Minicircle Data Edited/TFO'
-    path = '/Users/alice/Dropbox/UCL/DNA MiniCircles/Minicircle Data Edited/DNA/339/PLL'
-    # path = '/Users/alice/Dropbox/UCL/DNA MiniCircles/Code/GitTracing/Files'
+    # path = '/Users/alice/Dropbox/UCL/DNA MiniCircles/Minicircle Data Edited/DNA/339/PLL'
+    path = '/Users/alice/Dropbox/UCL/DNA MiniCircles/Code/GitTracing'
+    path = os.getcwd()
     # Set file type to look for here
     fileend = '.spm', '.jpk', '*.*[0-9]'
     filetype = '*.*[0-9]'
@@ -757,28 +559,8 @@ if __name__ == '__main__':
 
     # Concatenate statistics form all files into one dataframe for saving and plotting statistics
     grainstats_df = getdataforallfiles(appended_data)
-    # Search dataframes and return a new dataframe of only files containing a specific string
-    grainstats_searched = searchgrainstats(grainstats_df, 'filename', '339', 'nothing')
-
-    # # Plot all output from dataframe grainstats for initial visualisation as KDE plots
-    # plotall(grainstats_df, path, extension)
-
-    # Plot a single variable from the dataframe
-    # plotting(grainstats_df, 'grain_mean_radius', 'directory', bins, path, extension)
-    # plotting(grainstats_df, 'grain_bound_len', 'directory', bins, path, extension)
-    # plotting(grainstats_df, 'grain_max_bound_size', 'directory', bins, path, extension)
-    # plotting(grainstats_df, 'grain_min_bound_size', 'directory', bins, path, extension)
-
-    # Iterate through all keys in the grainstatsarguments file to plot various statistical quantities for a dataframe
-    # plottingallstats(grainstatsarguments, grainstats_df, extension, path)
-
-    # # Plot two variables from the dataframe - outputs both stacked by variable and full distributions
-    # plotting2(grainstats_df, 'grain_min_bound_size', 'grain_max_bound_size', 'directory', bins, path, extension)
-    # plotting2(grainstats_df, 'grain_mean', 'grain_maximum', 'directory', bins, path, extension)
-    # plotting2(grainstats_df, 'grain_maximum', 'grain_median', 'directory', bins, path, extension)
-
-    # # Plot a joint axis seaborn plot
-    # seaplotting(grainstats_searched, 'grain_min_bound_size', 'grain_max_bound_size', bins, path, extension)
+    # # Search dataframes and return a new dataframe of only files containing a specific string
+    # grainstats_searched = searchgrainstats(grainstats_df, 'filename', '339', 'nothing')
 
     # Saving stats to text and JSON files named by master path
     savestats(path, grainstats_df)
