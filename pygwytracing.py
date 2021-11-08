9f258258cba3b8f3a06c52e51863b5fea7e6ddf5
SylviaWhittle <sylviwhittle@gmail.com>
2021-11-08 14:26:27 +0000

    Added default config file
    Instead of running ```config.set``` multiple times to construct a new config file, a ```default_config.ini``` file is copied to ```config.ini```. This is easier to maintain and more convenient to edit the default parameters.

--- pygwytracing.py
+++ pygwytracing.py
@@ -29,6 +29,7 @@ import matplotlib.pyplot as plt
 import dnatracing
 import time
 import configparser
+from shutil import copyfile
 
 # Import height thresholding.py for processing bilayer removal images
 
@@ -705,74 +706,20 @@ if __name__ == '__main__':
     
     # Test if config file exists
     # If it doesn't, create a new config file with default parameters
-    if(not os.path.isfile("Config.ini")) :
-
-        print("No config file found named 'Config.ini'")
-        print("Creating Default Config File")
-
-        config = configparser.ConfigParser(allow_no_value=True)
-
-        # General parameters
-        config.add_section("MainSection")
-        config.set("MainSection", "; File path - the directory where the files are")
-        config.set("MainSection", "path", './')
-        config.set("MainSection", "; Set the sample type here")
-        config.set("MainSection", "sample_type","DNA")
-        config.set("MainSection", "; Set the file type to look for")
-        config.set("MainSection", "fileend", ".spm,.gwy,*.[0-9]")
-        config.set("MainSection", "filetype", '*.[0-9]')
-        config.set("MainSection", "; Set the extension of exported files")
-        config.set("MainSection", "extension", '.tiff')
-        config.set("MainSection", "; Set height scale values")
-        config.set("MainSection", "minheightscale", '-0e-9')
-        config.set("MainSection", "maxheightscale", '3e-9')
-        config.set("MainSection", "; Set the size of the cropped window / 2 in pixels")
-        config.set("MainSection", "cropwidth", '40e-9')
-        config.set("MainSection", "splitwidth", '2e-6')
-        config.set("MainSection", "; Set the number of bins")
-        config.set("MainSection", "bins", '25')
-
-        config.set("MainSection", "; Set the value of different valriables, based on the type of the sample.")
-        config.set("MainSection", "; Minarea is the minimum size for grain determination")
-        config.set("MainSection", "; Maxdeviation and mindeviation are the allowable deviations from the median pixel size for removal of large and small objects")
-
-        # DNA Specific parameters 
-        config.add_section("DNA")
-        config.set("DNA","minarea", '300e-9')
-        config.set("DNA","maxdeviation", '1.3')
-        config.set("DNA","mindeviation", '0.7')
-        config.set("DNA","gaussian", '0.1e-9')
-        config.set("DNA","thresholdingcriteria", '0.75')
-        
-        # Protein specific parameters
-        config.add_section("PROTIEN")
-        config.set("PROTIEN","minarea", '50e-9')
-        config.set("PROTIEN","maxdeviation", '2.0')
-        config.set("PROTIEN","mindeviation", '0.3')
-        config.set("PROTIEN","gaussian", '0.1e-9')
-        config.set("PROTIEN","thresholdingcriteria", '0.5')
-
-        # MAC specific parameters
-        config.add_section("MAC")
-        config.set("MAC","minarea", '1000e-9')
-        config.set("MAC","maxdeviation", '1.5')
-        config.set("MAC","mindeviation", '0.5')
-        config.set("MAC","gaussian", '0.25e-09')
-        config.set("MAC","thresholdingcriteria", '2.1')
-
-        # Write the config to file
-        cfgfile = open("Config.ini",'w')
-        config.write(cfgfile)
-        cfgfile.close()
+    if(not os.path.isfile("config.ini")) :
+        # Copy default_config.ini to config.ini
+        print("No config file found named 'config.ini'")
+        print("Copying default_config.ini to config.ini")
+        copyfile('default_config.ini','config.ini')
 
     # Read the config file
     print("Reading config file")
     config = configparser.ConfigParser()
-    config.read("Config.ini")
+    config.read("config.ini")
     print("Config loaded: ")
     print(" ")
 
-    # Print the current parameters
+    # Load and print the current parameters
 
     # Main section
     path = config.get("MainSection", "path")
