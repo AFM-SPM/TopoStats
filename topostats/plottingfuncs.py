404faa61f35ab21caa49356ca1a0f25d36df16ed
Sylvia <sylviwhittle@gmail.com>
2022-03-21 12:28:27 +0000

    Implement the new processing
    Implements the new processing, adding a main script 'topotracing.py', which calls upon 'filters.py' and 'plottingfuncs.py' to perform the initial stages of the old Gwyddion version of TopoStats.

---
+++ topostats/plottingfuncs.py
@@ -0,0 +1,11 @@
+from configparser import Interpolation
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_and_save(data, filename):
+    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+    if isinstance(data, np.ndarray):
+        ax.imshow(data, interpolation='nearest', cmap='afmhot')
+        plt.savefig(filename)
+    else:
+        data.show(ax=ax, Interpolation='nearest', cmap='afmhot')
