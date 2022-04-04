169d37bbf60f407173e86f2215ae2a876c77975a
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-04-04 13:07:47 +0100

    Implement the new processing
    Implements the new processing, adding a main script 'topotracing.py', which calls upon 'filters.py' and 'plottingfuncs.py' to perform the initial stages of the old Gwyddion version of TopoStats.

--- topostats/filters.py
+++ topostats/filters.py
@@ -1,3 +1,4 @@
+from statistics import median
 import numpy as np
 
 """Contains filter functions that take a 2D array representing an image as an input, as well as necessary parameters, and return a 2D array of the same size representing the filtered image."""
@@ -12,3 +13,39 @@ def amplify(image: np.array, level: float) -> np.array:
     """
 
     return image * level
+
+def row_col_quantiles(image: np.array, binary_mask=None) -> np.array:
+    """Returns the height value quantiles for the rows and columns.
+
+    :param image: A 2D raster image
+    :param binary_mask: (Array) Optional parameter that allows the use of a mask to ignore data
+    :return: Two arrays, one of row height value quantiles, the second of column height value quantiles
+    """
+
+    # Mask the data if applicable
+    if binary_mask != None:
+        image = np.ma.masked_array(image, mask=binary_mask, fill_value=np.nan)
+
+    # Initialise arrays
+    row_quantiles = np.zeros((image.shape[0], 3))
+    col_quantiles = np.zeros((image.shape[1], 3))
+
+    # Populate the row array with quantile tuples
+    for i in range(image.shape[0]):
+        row = image[i, :]
+        row_quantiles[i] = np.array([np.quantile(row, 0.25),
+        np.quantile(row, 0.5),
+        np.quantile(row, 0.75)])
+
+    # Populate the column array with quantile tuples
+    for j in range(image.shape[1]):
+        col = image[:, j]
+        col_quantiles = np.array([np.quantile(col, 0.25),
+        np.quantile(col, 0.5),
+        np.quantile(col, 0.75)])
+
+    return row_quantiles, col_quantiles
+
+
+
+
