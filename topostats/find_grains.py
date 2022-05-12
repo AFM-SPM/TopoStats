122 | Refactors grainstats.py and adds tests; corrects error in pixel scaling

topostats/find_grains.py
@@ -27,7 +27,7 @@ def quadratic(x, a, b, c):
 
 
 def gaussian_filter(
-    image: np.array, gaussian_size: float, pixel_nm_scaling: float, mode: str = "nearest", **kwargs
+    image: np.array, gaussian_size: float = 2, pixel_to_nm_scaling: float = 1, mode: str = "nearest", **kwargs
 ) -> np.array:
     """Apply Gaussian filter
 
@@ -37,7 +37,7 @@ def gaussian_filter(
         Numpy array of image.
     gaussian_size : float
         Gaussian blur size in nanometers (nm).
-    pixel_nm_scaling : float
+    pixel_to_nm_scaling : float
         Pixel to nanometer scale.
     mode : str
         Mode for filtering (default is 'nearest')
@@ -47,9 +47,12 @@ def gaussian_filter(
     np.array
         Numpy array of filtered image.
 
+    Examples
+    --------
+    FIXME: Add docs.
     """
-    LOGGER.info("Applying Gaussian filter (mode : {mode}; Gaussian blur (nm) : {gaussian_size}).")
-    return gaussian(image, sigma=(gaussian_size / pixel_nm_scaling), mode=mode, **kwargs)
+    LOGGER.info(f"Applying Gaussian filter (mode : {mode}; Gaussian blur (nm) : {gaussian_size}).")
+    return gaussian(image, sigma=(gaussian_size * pixel_to_nm_scaling), mode=mode, **kwargs)
 
 
 def tidy_border(image: np.array, **kwargs) -> np.array:
@@ -86,13 +89,13 @@ def label_regions(image: np.array, background: float = 0.0, **kwargs) -> np.arra
     return label(image, background=background, **kwargs)
 
 
-def minimum_grain_size_pixels(image: np.array, background: float) -> float:
-    """Calculate the minimum grain size of interest in pixels.
+def calc_minimum_grain_size(image: np.array, background: float) -> float:
+    """Calculate the minimum grain size.
 
     Parameters
     ----------
     image : np.array
-        Numpy array of regions of interest labelled.
+        Numpy array of regions of interest after labelling.
     background : float
 
     Returns
@@ -100,13 +103,12 @@ def minimum_grain_size_pixels(image: np.array, background: float) -> float:
     float
         Threshold for minimum grain size.
     """
-    labelled_data = label_regions(image, background)
-    grain_areas = np.array([grain.area for grain in region_properties(labelled_data)])
+    grain_areas = np.array([grain.area for grain in region_properties(image)])
     grain_areas = grain_areas[grain_areas > get_threshold(grain_areas)]
     return np.median(grain_areas) - (1.5 * (np.quantile(grain_areas, 0.75) - np.quantile(grain_areas, 0.25)))
 
 
-def remove_objects(image: np.array, minimum_grain_size_pixels: float, **kwargs) -> np.array:
+def remove_objects(image: np.array, minimum_grain_size_pixels: float, pixel_to_nm_scaling: float, **kwargs) -> np.array:
     """Remove small objects
 
     Parameters
@@ -115,14 +117,15 @@ def remove_objects(image: np.array, minimum_grain_size_pixels: float, **kwargs) 
         Numpy array representing image.
     minimum_grain_size_pixels: float
         Minimum grain size in pixels.
-
+    pixel_to_nm_scaling: float
+        Pixel to nanometer scale.
     Returns
     -------
     np.array
         Numpy array of image with objects coloured.
     """
     LOGGER.info(f"Removing small objects (< {minimum_grain_size_pixels})")
-    return remove_small_objects(image, min_size=minimum_grain_size_pixels, **kwargs)
+    return remove_small_objects(image, min_size=minimum_grain_size_pixels * pixel_to_nm_scaling, **kwargs)
 
 
 def colour_regions(image: np.array, **kwargs) -> np.array:
@@ -171,6 +174,12 @@ def get_bounding_boxes(region_prop: List) -> Dict:
     -------
     dict
         Dictionary of bounding boxes indexed by region area.
+
+
+    Examples
+    --------
+    FIXME: Add docs.
+
     """
     LOGGER.info("Extracting bounding boxes")
     return {region.area: region.area_bbox for region in region_prop}
@@ -184,7 +193,6 @@ def save_region_stats(bounding_boxes: dict, output_dir: Union[str, Path]) -> Non
     bounding_boxes: dict
         Dictionary of bounding boxes
     output_dir: Union[str, Path]
-        Where to save the statistics to as a CSV file called 'grainstats.csv'.
-    """
+        Where to save the statistics to as a CSV file called 'grainstats.csv'."""
     grainstats = pd.DataFrame.from_dict(data=bounding_boxes, orient="index")
     grainstats.to_csv(output_dir / "grainstats.csv", index=True)