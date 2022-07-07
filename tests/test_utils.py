d4fc1e28a6bd85b195611ed14683ed1ab84b2ff8
Neil Shephard <n.shephard@sheffield.ac.uk>
2022-07-07 09:47:33 +0100

    165 | Expanding test_utils.py for thresholding

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,10 +4,13 @@ from pathlib import Path
 import numpy as np
 
 from topostats.thresholds import threshold
-from topostats.utils import convert_path, find_images, update_config, get_mask
+from topostats.utils import convert_path, find_images, update_config, get_mask, get_thresholds
 
 
-def test_convert_path(tmpdir):
+THRESHOLD_OPTIONS = {"otsu_threshold_multiplier": 1.7, "deviation_from_mean": 1, "absolute": 1.5}
+
+
+def test_convert_path(tmpdir) -> None:
     """Test path conversion."""
     test_dir = str(tmpdir)
     converted_path = convert_path(test_dir)
@@ -16,7 +19,7 @@ def test_convert_path(tmpdir):
     assert tmpdir == converted_path
 
 
-def test_find_images():
+def test_find_images() -> None:
     """Test finding images"""
     found_images = find_images(base_dir="tests/", file_ext=".spm")
 
@@ -26,7 +29,7 @@ def test_find_images():
     assert "minicircle.spm" in str(found_images[0])
 
 
-def test_update_config(caplog):
+def test_update_config(caplog) -> None:
     """Test updating configuration."""
     SAMPLE_CONFIG = {"a": 1, "b": 2, "c": "something", "base_dir": "here", "output_dir": "there"}
     NEW_VALUES = {"c": "something new"}
@@ -35,3 +38,26 @@ def test_update_config(caplog):
     assert isinstance(updated_config, dict)
     assert "Updated config config[c] : something > something new" in caplog.text
     assert updated_config["c"] == "something new"
+
+
+def test_get_thresholds_otsu(image_random: np.ndarray, caplog) -> None:
+    """Test of get_thresholds() method otsu threshold."""
+    thresholds = get_thresholds(image=image_random, threshold_method="otsu", **THRESHOLD_OPTIONS)
+    print(f"thresholds : {thresholds}")
+    assert isinstance(thresholds, dict)
+    assert thresholds = {"upper": 0.8466799787547299}
+
+
+def test_get_thresholds_mean(image_random: np.ndarray, caplog) -> None:
+    """Test of get_thresholds() method with mean threshold."""
+    thresholds = get_thresholds(image=image_random, threshold_method="mean", **THRESHOLD_OPTIONS)
+    assert isinstance(thresholds, dict)
+    assert thresholds = {"upper": 0.8466799787547299}
+    assert False
+
+def test_get_thresholds_absolute(image_random: np.ndarray, caplog) -> None:
+    """Test of get_thresholds() method with absolute threshold."""
+    thresholds = get_thresholds(image=image_random, threshold_method="absolute", **THRESHOLD_OPTIONS)
+    print(f"thresholds : {thresholds}")
+    assert isinstance(thresholds, dict)
+    assert False
