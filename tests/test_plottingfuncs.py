afe7aeecaf484f0d15d86181567e425b7ae2dff5
Neil Shephard <n.shephard@sheffield.ac.uk>
2022-07-14 13:20:33 +0100

    212 | Test for bounding boxes added

--- tests/test_plottingfuncs.py
+++ tests/test_plottingfuncs.py
@@ -2,6 +2,7 @@
 import pytest
 
 from topostats.filters import Filters
+from topostats.grains import Grains
 from topostats.plottingfuncs import plot_and_save
 
 
@@ -30,5 +31,24 @@ def test_plot_and_save_colorbar_afmhot(minicircle_pixels: Filters, plotting_conf
     plotting_config["cmap"] = "afmhot"
     fig, _ = plot_and_save(
         minicircle_pixels.images["pixels"], tmpdir, "01-raw_heightmap.png", title="Raw Height", **plotting_config
+    )
+    return fig
+
+
+@pytest.mark.mpl_image_compare(baseline_dir="resources/img/")
+def test_plot_and_save_bounding_box(
+    minicircle_grain_coloured: Grains,
+    minicircle_grain_region_properties_post_removal: Grains,
+    plotting_config: dict,
+    tmpdir,
+) -> None:
+    """Test plotting bounding boxes"""
+    fig, _ = plot_and_save(
+        minicircle_grain_coloured.directions["upper"]["coloured_regions"],
+        tmpdir,
+        "15-coloured_regions.png",
+        title="Coloured Regions",
+        **plotting_config,
+        region_properties=minicircle_grain_region_properties_post_removal
     )
     return fig
