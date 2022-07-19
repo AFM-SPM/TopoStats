359914b98bbf8b06937c4cab455b6d88234c6c5d
Neil Shephard <n.shephard@sheffield.ac.uk>
2022-07-19 18:13:18 +0100

    222 | Adding test for colorbar = False

--- tests/test_plottingfuncs.py
+++ tests/test_plottingfuncs.py
@@ -24,6 +24,15 @@ def test_plot_and_save_colorbar(minicircle_pixels: Filters, plotting_config: dic
     )
     return fig
 
+@pytest.mark.mpl_image_compare(baseline_dir="resources/img/")
+def test_plot_and_save_colorbar_false(minicircle_pixels: Filters, plotting_config: dict, tmpdir) -> None:
+    """Test plotting without colorbar"""
+    plotting_config["colorbar"] = False
+    fig, _ = plot_and_save(
+        minicircle_pixels.images["pixels"], tmpdir, "01-raw_heightmap.png", title="Raw Height", **plotting_config
+    )
+    return fig
+
 
 @pytest.mark.mpl_image_compare(baseline_dir="resources/img/")
 def test_plot_and_save_colorbar_afmhot(minicircle_pixels: Filters, plotting_config: dict, tmpdir) -> None:
