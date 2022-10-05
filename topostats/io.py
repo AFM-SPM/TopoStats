c7a3268f290d1a577ef6e59d70c9fc3831b96ae6
Max Gamill <mcgamill1@sheffield.ac.uk>
2022-10-05 15:10:14 +0100

    File loading moved to from `Filters` `io`

--- topostats/io.py
+++ topostats/io.py
@@ -3,6 +3,7 @@ import logging
 from datetime import datetime
 from pathlib import Path
 from typing import Union, Dict
+import numpy as np
 
 from pySPM.Bruker import Bruker
 from ruamel.yaml import YAML, YAMLError
@@ -59,21 +60,75 @@ def write_yaml(config: dict, output_dir: Union[str, Path]) -> None:
         except YAMLError as exception:
             LOGGER.error(exception)
 
+class Load_scan():
+    """Load the image and image parameters from a file path."""
 
-def load_scan(img_path: Union[str, Path]) -> Bruker:
-    """Load the image from file.
+    def __init__(
+        self,
+        img_path: Union[str, Path],
+        channel: str,
+        ):
 
-    Parameters
-    ----------
-    img_path : Union[str, Path]
-        Path to image that needs loading.
+        """Initialise the class.
 
-    Returns
-    -------
-    Bruker
+        Parameters
+        ----------
+        img_path: Union[str, Path]
+            Path to a valid AFM scan to load.
+        channel: str
+            Image channel to extract from the scan.
+        """
+        self.img_path = Path(img_path)
+        self.channel = channel
+    
+    def extract_filename(self) -> str:
+        """Extract the filename from the image path"""
+        LOGGER.info(f"Extracting filename from : {self.img_path}")
+        self.filename = self.img_path.stem
+        return self.filename
 
-    Examples
-    --------
-    """
-    LOGGER.info(f"Loading image from : {img_path}")
-    return Bruker(Path(img_path))
+    def spm(self) -> tuple:
+        """Extract the image and px_to_nm_scaling value from the .spm file."""
+        LOGGER.info(f"Loading image from : {self.img_path}")
+        try:
+            scan = Bruker(self.img_path)
+            LOGGER.info(f"[{self.filename}] : Loaded image from : {self.img_path}")
+            channel_data = scan.get_channel(self.channel)
+            LOGGER.info(f"[{self.filename}] : Extracted channel {self.channel}")
+            self.image = np.flipud(np.array(channel_data.pixels))
+        except FileNotFoundError:
+            LOGGER.info(f"File not found : {self.img_path}")
+        except Exception as exception:
+            LOGGER.error(f"[{self.filename}] : {exception}")
+
+        unit_dict = {
+            "nm": 1,
+            "um": 1e3,
+        }
+        px_to_real = channel_data.pxs()
+        # Has potential for non-square pixels but not yet implimented
+        self.px_to_m_scaling = (
+            px_to_real[0][0] * unit_dict[px_to_real[0][1]],
+            px_to_real[1][0] * unit_dict[px_to_real[1][1]],
+        )[0]
+        LOGGER.info(f"[{self.filename}] : Pixels to nm scaling : {self.px_to_m_scaling}")
+        return (self.image, self.px_to_m_scaling)
+
+    def get_data(self) -> tuple:
+        """The factory method using different libraries to obtain the:
+            Filename without extentions - for logging and saving plots
+            Image as an array in SI units *tbd*
+            px_to_nm_scaling in SI units *tbd*
+        """
+        filename = Load_scan.extract_filename(self)
+        suffix = self.img_path.suffix
+        if suffix == ".spm":
+            LOGGER.info(f"Using pySPM to extract image")
+            image, px_to_m_scaling = Load_scan.spm(self)
+        if suffix == ".jpk":
+            LOGGER.info(f"Using libasd to extract image")
+            #image, px_to_m_scaling = Load_scan.jpk(self)
+        if suffix == ".ibw":
+            LOGGER.info(f"Using afmformats to extract image")
+            #image, px_to_m_scaling = Load_scan.ibw(self)
+        return (filename, image, px_to_m_scaling)
