4863da1f0a1998cb8e712f5d3a1dcfc7a0dee209
Neil Shephard <n.shephard@sheffield.ac.uk>
2022-10-05 10:34:51 +0100

    299 | Improved validation error, exits reporting errors.

--- topostats/validation.py
+++ topostats/validation.py
@@ -1,7 +1,7 @@
 """Validation of configuration."""
 import logging
 from pathlib import Path
-from schema import And, Or, Schema, SchemaError
+from schema import Or, Schema, SchemaError
 
 from topostats.logs.logs import LOGGER_NAME
 
@@ -20,19 +20,23 @@ def validate_config(config: dict):
         {
             "base_dir": Path,
             "output_dir": Path,
-            "warnings": Or("ignore"),
+            "warnings": Or("ignore", error="Invalid value in config for 'warnings', valid values are 'ignore'"),
             "cores": lambda n: 1 <= n <= 16,
-            "quiet": Or(True, False),
-            "file_ext": Or(".spm", ".jpk", error="Invalid value for file_ext, valid values are '.spm' or '.jpk'"),
+            "quiet": Or(True, False, error="Invalid value in config for 'quiet', valid values are 'True' or 'False'"),
+            "file_ext": Or(
+                ".spm", ".jpk", error="Invalid value in config for 'file_ext', valid values are '.spm' or '.jpk'"
+            ),
             "filter": {
-                "run": Or(True, False, error="Invalid value for filter.run, valid values are True or False"),
+                "run": Or(
+                    True, False, error="Invalid value in config for 'filter.run', valid values are 'True' or 'False'"
+                ),
                 "channel": Or("Height"),
                 "amplify_level": float,
                 "threshold_method": Or(
                     "absolute",
                     "otsu",
                     "std_dev",
-                    error="Invalid value for filter.threshold_method, valid values are 'absolute', 'otsu' or 'std_dev'",
+                    error="Invalid value in config for 'filter.threshold_method', valid values are 'absolute', 'otsu' or 'std_dev'",  # pylint: disable=line-too-long
                 ),
                 "otsu_threshold_multiplier": float,
                 "threshold_std_dev": lambda n: 0 < n <= 6,
@@ -40,17 +44,19 @@ def validate_config(config: dict):
                 "threshold_absolute_upper": float,
                 "gaussian_size": float,
                 "gaussian_mode": Or(
-                    "nearest", error="Invalid value for filter.gaussian_mode, valid values are 'nearest'"
+                    "nearest", error="Invalid value in config for 'filter.gaussian_mode', valid values are 'nearest'"
                 ),
             },
             "grains": {
-                "run": Or(True, False, error="Invalid value for grains.run, valid values are True or False"),
+                "run": Or(
+                    True, False, error="Invalid value in config for grains.run, valid values are 'True' or 'False'"
+                ),
                 "absolute_smallest_grain_size": int,
                 "threshold_method": Or(
                     "absolute",
                     "otsu",
                     "std_dev",
-                    error="Invalid value for filter.threshold_method, valid values are 'absolute', 'otsu' or 'std_dev'",
+                    error="Invalid value in config for 'grains.threshold_method', valid values are 'absolute', 'otsu' or 'std_dev'",  # pylint: disable=line-too-long
                 ),
                 "otsu_threshold_multiplier": float,
                 "threshold_std_dev": lambda n: 0 < n <= 6,
@@ -61,14 +67,14 @@ def validate_config(config: dict):
                         Or(
                             int,
                             None,
-                            error="Invalid value for grains.absolute_area_threshold.upper, valid values are int or null",
+                            error="Invalid value in config for 'grains.absolute_area_threshold.upper', valid values are int or null",  # pylint: disable=line-too-long
                         )
                     ],
                     "lower": [
                         Or(
                             int,
                             None,
-                            error="Invalid value for grains.absolute_area_threshold.upper, valid values are int or null",
+                            error="Invalid value in config for 'grains.absolute_area_threshold.lower', valid values are int or null",  # pylint: disable=line-too-long
                         )
                     ],
                 },
@@ -81,28 +87,46 @@ def validate_config(config: dict):
                 "background": float,
             },
             "grainstats": {
-                "run": Or(True, False, error="Invalid value for filter.run, valid values are True or False"),
+                "run": Or(
+                    True,
+                    False,
+                    error="Invalid value in config for 'grainstats.run', valid values are 'True' or 'False'",
+                ),
                 "cropped_size": float,
                 "save_cropped_grains": Or(
-                    True, False, error="Invalid value for filter.run, valid values are True or False"
+                    True,
+                    False,
+                    error="Invalid value in config for 'grainstats.save_cropped_grains, valid values are 'True' or 'False'",  # pylint: disable=line-too-long
                 ),
             },
             "dnatracing": {
-                "run": Or(True, False, error="Invalid value for filter.run, valid values are True or False")
+                "run": Or(
+                    True, False, error="Invalid value in config for 'filter.run', valid values are 'True' or 'False'"
+                )
             },
             "plotting": {
-                "run": Or(True, False),
+                "run": Or(
+                    True, False, error="Invalid value in config for 'plotting.run', valid values are 'True' or 'False'"
+                ),
                 "save_format": str,
                 "image_set": Or(
-                    "all", "core", error="Invalid value for plotting.image_set, valid values are 'all' or 'core'"
+                    "all",
+                    "core",
+                    error="Invalid value in config for 'plotting.image_set', valid values are 'all' or 'core'",
                 ),
                 "zrange": list,
-                "colorbar": Or(True, False, error="Invalid value for filter.run, valid values are True or False"),
-                "axes": Or(True, False, error="Invalid value for filter.run, valid values are True or False"),
+                "colorbar": Or(
+                    True,
+                    False,
+                    error="Invalid value in config for 'plotting.colorbar', valid values are 'True' or 'False'",
+                ),
+                "axes": Or(
+                    True, False, error="Invalid value in config plotting.for 'axes', valid values are 'True' or 'False'"
+                ),
                 "cmap": Or(
                     "afmhot",
                     "nanoscope",
-                    error="Invalid value for plotting.cmap, valid values are 'afmhot' or 'nanoscope",
+                    error="Invalid value in config for 'plotting.cmap', valid values are 'afmhot' or 'nanoscope",
                 ),
             },
         }
@@ -112,7 +136,6 @@ def validate_config(config: dict):
         config_schema.validate(config)
         LOGGER.info("Configuration is valid.")
     except SchemaError as se:
-        print(f"schemaerror : {se}")
-        for error in se.errors:
-            #            if error:
-            LOGGER.error(error)
+        raise SchemaError(
+            "There is an error in your configuration. Please refer to the first error message above for details"
+        ) from se
