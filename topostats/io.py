794475ae6d0a122a04be4a977f5342568050a4b4
Max Gamill <mcgamill1@sheffield.ac.uk>
2022-10-19 18:28:03 +0100

    Redo input for path list & output an iterable dict

--- topostats/io.py
+++ topostats/io.py
@@ -7,8 +7,8 @@ import numpy as np
 
 from pySPM.Bruker import Bruker
 from afmformats import load_data
+import libasd
 from afmformats.mod_creep_compliance import AFMCreepCompliance
-
 from ruamel.yaml import YAML, YAMLError
 from ruamel.yaml.main import round_trip_load as yaml_load, round_trip_dump as yaml_dump
 from topostats.logs.logs import LOGGER_NAME
@@ -65,7 +65,7 @@ def write_yaml(config: dict, output_dir: Union[str, Path]) -> None:
         except YAMLError as exception:
             LOGGER.error(exception)
 
-
+'''
 class LoadScan:
     """Load the image and image parameters from a file path."""
 
@@ -104,7 +104,6 @@ class LoadScan:
             image = np.flipud(np.array(self.channel_data.pixels))
         except FileNotFoundError:
             LOGGER.info(f"[{self.filename}] File not found : {self.img_path}")
-        # check exception of channel not found and return channel list
         except Exception as exception:
             LOGGER.error(f"[{self.filename}] : {exception}")
 
@@ -151,11 +150,7 @@ class LoadScan:
             LOGGER.error(f"[{self.filename}] : {exception}")
         return jpk
 
-    @staticmethod
-    def _extract_jpk(jpk: AFMCreepCompliance) -> np.ndarray:
-        """Extract data from jpk object"""
-
-    def extract_asd(self) -> tuple:
+    def load_asd(self) -> tuple:
         """Extract image and pixel to nm scaling from .asd files"""
         # asd files are like movies so may want this outside of process scan?
         try:
@@ -177,12 +172,18 @@ class LoadScan:
         except ValueError:
             LOGGER.info(f"[{self.filename}] : Channel '{self.channel}' not found. Available channels are: '{ch1_name}' and '{ch2_name}'")
 
+        return (image, self._asd_px_to_nm_scaling(scan))
+
+    def _asd_px_to_nm_scaling(self, scan):
         px_to_m_scaling = (
             scan.header.x_scanning_range / scan.header.x_pixel, # scan range always in nm
             scan.header.y_scanning_range / scan.header.y_pixel,
         )[0]
-        return (image, px_to_nm_scaling)
+        return px_to_m_scaling * 1e9
 
+    @staticmethod
+    def _extract_jpk(jpk: AFMCreepCompliance) -> np.ndarray:
+        """Extract data from jpk object"""
 
     def get_data(self) -> None:
         """Method to extract image and pixel to nm scaling."""
@@ -193,3 +194,180 @@ class LoadScan:
             self.image, self.pixel_to_nm_scaling = self.load_jpk()
         if self.suffix == ".ibw":
             self.image, self.pixel_to_nm_scaling = self.load_ibw()
+
+
+class DataLoader:
+    def __init__(self):    
+        self.data_dict = {
+            "images": [],
+            "px_2_nm": [],
+            "filename": []
+        }
+
+    def dataloader(self, image, pixel_to_nm_scaling, filename) -> None:
+        "Compiles all file outputs into a single dictionary"
+        self.data_dict["images"].append(image)
+        self.data_dict["px_2_nm"].append(pixel_to_nm_scaling)
+        self.data_dict["filename"].append(filename)
+
+'''
+
+
+class LoadScans:
+    """Load the image and image parameters from a file path."""
+
+    def __init__(
+        self,
+        img_paths: list,
+        channel: str,
+    ):
+
+        """Initialise the class.
+
+        Parameters
+        ----------
+        img_path: Union[str, Path]
+            Path to a valid AFM scan to load.
+        channel: str
+            Image channel to extract from the scan.
+        """
+        self.img_paths = img_paths
+        self.img_path = None
+        self.channel = channel
+        self.channel_data = None
+        self.filename = None
+        self.suffix = None
+        self.image = None
+        self.pixel_to_nm_scaling = None
+        self.img_dic = {
+            "images": [],
+            "img_paths": [],
+            "px_2_nms": []
+        }
+
+    def load_spm(self) -> tuple:
+        """Extract image and pixel to nm scaling from the Bruker .spm file."""
+        LOGGER.info(f"Loading image from : {self.img_path}")
+        try:
+            scan = Bruker(self.img_path)
+            LOGGER.info(f"[{self.filename}] : Loaded image from : {self.img_path}")
+            self.channel_data = scan.get_channel(self.channel)
+            LOGGER.info(f"[{self.filename}] : Extracted channel {self.channel}")
+            image = np.flipud(np.array(self.channel_data.pixels))
+        except FileNotFoundError:
+            LOGGER.info(f"[{self.filename}] File not found : {self.img_path}")
+        except Exception as exception:
+            LOGGER.error(f"[{self.filename}] : {exception}")
+
+        return (image, self._spm_pixel_to_nm_scaling(self.channel_data))
+
+    def _spm_pixel_to_nm_scaling(self, channel_data) -> float:
+        """Extract pixel to nm scaling from the SPM image metadata.
+
+        Parameters
+        ----------
+        channel_data:
+            Channel data
+
+        Returns
+        -------
+        float
+            Pixel to nm scaling factor.
+        """
+        unit_dict = {
+            "nm": 1,
+            "um": 1e3,
+        }
+        px_to_real = channel_data.pxs()
+        # Has potential for non-square pixels but not yet implimented
+        pixel_to_nm_scaling = (
+            px_to_real[0][0] * unit_dict[px_to_real[0][1]],
+            px_to_real[1][0] * unit_dict[px_to_real[1][1]],
+        )[0]
+        LOGGER.info(f"[{self.filename}] : Pixel to nm scaling : {pixel_to_nm_scaling}")
+        return pixel_to_nm_scaling
+
+    # def load_jpk(self) -> None:
+    #     """Load and extract image from .jpk files."""
+    #     jpk = self._load_jpk()
+    #     data = self._extract_jpk(jpk)
+    #     return (jpk, None)
+
+    def _load_jpk(self) -> None:
+        try:
+            jpk = load_data(self.img_path)
+        except FileNotFoundError:
+            LOGGER.info(f"[{self.filename}] File not found : {self.img_path}")
+        except Exception as exception:
+            LOGGER.error(f"[{self.filename}] : {exception}")
+        return jpk
+
+    def load_asd(self) -> tuple:
+        """Extract image and pixel to nm scaling from .asd files"""
+        # asd files are like movies so may want this outside of process scan?
+        try:
+            scan = libasd.read_asd(self.img_path)
+            LOGGER.info(f"[{self.filename}] : Loaded image from : {self.img_path}")
+        except FileNotFoundError:
+            LOGGER.info(f"[{self.filename}] File not found : {self.img_path}")
+        try:
+            # libasd docs seem like there is only 2 channels i.e. help(scan)
+            # libasd.Header_v0 uses "type" and _v1 uses "kind"
+            ch1_name = str(scan.header.data_type_1ch).split('.')[1]
+            ch2_name = str(scan.header.data_type_2ch).split('.')[1]
+        except AttributeError:
+            ch1_name = str(scan.header.data_kind_1ch).split('.')[1]
+            ch2_name = str(scan.header.data_kind_2ch).split('.')[1]
+        try:
+            if self.channel == ch1_name:
+                channel_data = scan.channels[0]
+            elif self.channel == ch2_name:
+                channel_data = scan.channels[1]
+            else:
+                raise ValueError
+            LOGGER.info(f"[{self.filename}] : Extracted channel {self.channel} with {len(channel_data)} frames")
+            images = [channel_data[i].image() for i in range(len(channel_data))]
+        except ValueError:
+            LOGGER.info(f"[{self.filename}] : Channel '{self.channel}' not found. Available channels are: '{ch1_name}' and '{ch2_name}'")
+            raise ValueError(f"[{self.filename}] : Channel '{self.channel}' not found. Available channels are: '{ch1_name}' and '{ch2_name}'")
+
+        return (images, self._asd_px_to_nm_scaling(scan))
+
+    def _asd_px_to_nm_scaling(self, scan):
+        px_to_m_scaling = (
+            scan.header.x_scanning_range / scan.header.x_pixel, # scan range always in nm
+            scan.header.y_scanning_range / scan.header.y_pixel,
+        )[0]
+        return px_to_m_scaling * 1e9
+
+    @staticmethod
+    def _extract_jpk(jpk: AFMCreepCompliance) -> np.ndarray:
+        """Extract data from jpk object"""
+
+    def get_data(self) -> None:
+        """Method to extract image and pixel to nm scaling."""
+        for img_path in self.img_paths:
+            self.img_path = img_path
+            self.filename = img_path.stem
+            suffix = img_path.suffix
+            LOGGER.info(f"Extracting image from {self.img_path}")
+            if suffix == ".spm":
+                self.image, self.pixel_to_nm_scaling = self.load_spm()
+                self.add_to_dic(self.image, self.filename, self.pixel_to_nm_scaling)
+            if suffix == ".jpk":
+                self.image, self.pixel_to_nm_scaling = self.load_jpk()
+            if suffix == ".ibw":
+                self.image, self.pixel_to_nm_scaling = self.load_ibw()
+            if suffix == ".asd":
+                self.img_path = str(self.img_path)
+                self.image, self.pixel_to_nm_scaling = self.load_asd()
+                for i, frame in enumerate(self.image):
+                    pathname = list(Path(self.img_path).parts)
+                    pathname[-1] = self.filename+"_frame_"+str(i)
+                    pathname = Path(*pathname)
+                    self.add_to_dic(frame, pathname, self.pixel_to_nm_scaling)
+
+    def add_to_dic(self, image, img_path, px_2_nm):
+        self.img_dic["images"].append(image)
+        self.img_dic["img_paths"].append(img_path)
+        self.img_dic["px_2_nms"].append(px_2_nm)
