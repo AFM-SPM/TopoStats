de691aaee085988da38333432b0ecb4f59a2fef6
Max Gamill <mcgamill1@sheffield.ac.uk>
2022-12-04 20:40:03 +0000

    tests: - dnatrace skeleton, + binary img to dict

--- tests/tracing/test_dnatracing.py
+++ tests/tracing/test_dnatracing.py
@@ -8,10 +8,10 @@ from topostats.tracing.dnatracing import dnaTrace, traceStats
 
 # pylint: disable=protected-access
 
+# keeping target dictionary as may want to use these grains for more tests
 
-def test_get_grain_array(test_dnatracing: pd.DataFrame) -> None:
-    """Test extraction of grain array."""
-    target = {
+mask_n_points = {
+    "masks": {
         1: np.array(
             [
                 [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
@@ -64,49 +64,38 @@ def test_get_grain_array(test_dnatracing: pd.DataFrame) -> None:
                 [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
             ]
         ),
-    }
-    for x in range(1, 4):
-        np.testing.assert_array_equal(test_dnatracing._get_grain_array(x), target[x])
+        },
+    "points": {
+        1: "a"
+        }}
+
+multigrain = np.array(
+    [
+        [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2],
+        [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 2],
+        [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 2],
+        [0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [0, 0, 3, 3, 3, 3, 0, 0, 0, 0, 0],
+        [3, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0],
+    ])
 
 
 @pytest.mark.parametrize(
-    "molecule, array_sum",
-    [
-        (1, 2872),
-        (2, 1726),
-        (3, 1770),
-        (4, 1624),
-        (5, 1706),
-        (6, 1695),
-        (7, 1729),
-        (8, 1855),
-        (9, 2085),
-        (10, 1900),
-        (11, 2054),
-        (12, 2003),
-        (13, 1919),
-        (14, 1887),
-        (15, 1766),
-        (16, 1898),
-        (17, 1965),
-        (18, 2000),
-        (19, 1818),
-        (20, 1967),
-        (21, 1895),
-        (22, 1826),
-    ],
+    "multi_grain, no_grains",
+    [(multigrain, 3)],
 )
-def test_minicircle_dnatracing_skeleton_size(regtest, minicircle_dnatracing: traceStats, molecule, array_sum) -> None:
-    """Test the skeletons returned by dnatracing."""
-    assert minicircle_dnatracing._get_grain_array(molecule).sum() == array_sum
-    mol_array = minicircle_dnatracing._get_grain_array(1)
-
-
-def test_minicircle_dnatracing_skeletons(regtest, minicircle_dnatracing: traceStats) -> None:
-    """Test the skeletons returned by dnatracing."""
-    mol_array = minicircle_dnatracing._get_grain_array(1)
-    assert isinstance(mol_array, np.ndarray)
-    print(np.array2string(mol_array), file=regtest)
+def test_binary_img_to_dict(multi_grain: np.ndarray, no_grains: int) -> None:
+    """Test that a binary image can be separated into arrays."""
+    mask = multi_grain.copy()
+    mask[mask !=0 ] = 0
+    test_dict = dnaTrace.binary_img_to_dict(multi_grain)
+    assert len(test_dict) == no_grains
+    for grain in range(1, no_grains + 1):
+        assert test_dict[grain].shape == multi_grain.shape
+        assert (test_dict[grain] == np.where(multi_grain == grain, 1, 0)).all
 
 
 # @pytest.mark.mpl_image_compare(baseline_dir="../resources/img/")
