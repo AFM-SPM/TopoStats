f694db8f86b83f4505ff05fd16fb0cc7bcce1c0d
SylviaWhittle <sylviwhittle@gmail.com>
2022-12-06 11:06:19 +0000

    Add more comprehensive documentation to scars.py

--- topostats/scars.py
+++ topostats/scars.py
@@ -26,8 +26,41 @@ class Scars:
         threshold_high: float,
         max_scar_width: int,
         min_scar_length: int,
-    ):
-        """Initialise the class"""
+    ) -> None:
+        """
+        Initialise the class
+       
+        Parameters
+        ----------
+        img: np.ndarray
+            A 2-D image to remove scars from.
+        filename: str
+            The filename (used for logging outputs only).
+        run: bool
+            A toggle to turn scar removal on, or off.
+        removal_iterations: int
+            The number of times the scar removal should run on the image.
+            Running just once sometimes isn't enough to remove some of the
+            more difficult to remove scars.
+        threshold_low: float
+            A value that when multiplied with the standard deviation, acts as a threshold to determine if an increase
+            or decrease in height might constitute the top or bottom of a scar.
+        threshold_high: float
+            A floating point value that is used similarly to threshold_low, however sharp inclines or descents
+            that result in values in the mask higher than this threshold are automatically considered scars.
+        max_scar_width: int
+            A value that dictates the maximum width that a scar can be. Note that this does not mean horizontal width,
+            rather vertical, this is because we conside scars to be laying flat, horizontally, so their width is
+            vertical and their length is horizontal.
+        min_scar_length: int
+            An integer that restricts the algorithm to only mark scars taht are as long or longer than this length.
+            This is important for ensuring that noise or legitimate but sharp datapoints do not get detected as scars.
+            Note that length here is horizontal, as scars are thin, horizontal features.
+
+        Returns
+        -------
+        None
+        """
         self.img = img
         self.filename = filename
         self.run = run
@@ -135,6 +168,7 @@ class Scars:
 
         Returns
         -------
+        None
         """
 
         # Unpack row, col
@@ -169,6 +203,26 @@ class Scars:
         any pixels that are parts of scars that might not have been extreme enough to get marked above the
         high_threshold.
         Any remaining marked pixels below high_threshold are considered not to be scars and are removed from the mask.
+
+        Parameters
+        ----------
+        marked: np.ndarray
+            A 2-D image of pixels that stores the positions of scars marked for removal. The value of the pixel is a
+            floating point value that represents how strongly the algorithm considers it to be a scar.
+            This may or may not already contain non-zero values given that previous iterations of scar removal
+            may have been performed.
+        threshold_low: float
+            A floating point value, that when multiplied by the standard deviation of the image, acts as a
+            threshold for sharp inclines or descents in pixel values and thus marks potential scars.
+            A lower value will make the algorithm
+            more sensitive, and a higher value will make it less sensitive.
+        threshold_high: float
+            A floating point value that is used similarly to threshold_low, however sharp inclines or descents
+            that result in values in the mask higher than this threshold are automatically considered scars.
+
+        Returns
+        -------
+        None
         """
 
         # Spread scars that have close to threshold edge-points
@@ -189,6 +243,27 @@ class Scars:
     def remove_too_short_scars(self, marked: np.ndarray, threshold_high: float, min_scar_length: int) -> None:
         """
         Removes scars that are too short (horizontally), based on the min_scar_length argument.
+
+        Parameters
+        ----------
+        marked: np.ndarray
+            A 2-D image of pixels that stores the positions of scars marked for removal. The value of the pixel is a
+            floating point value that represents how strongly the algorithm considers it to be a scar.
+            This may or may not already contain non-zero values given that previous iterations of scar removal
+            may have been performed.
+
+        threshold_high: float
+            A floating point value that is used similarly to threshold_low, however sharp inclines or descents
+            that result in values in the mask higher than this threshold are automatically considered scars.
+
+        min_scar_length: int
+            A value that dictates the maximum width that a scar can be. Note that this does not mean horizontal width,
+            rather vertical, this is because we conside scars to be laying flat, horizontally, so their width is
+            vertical and their length is horizontal.
+
+        Returns
+        -------
+        None
         """
 
         # Remove too-short scars
@@ -251,13 +326,15 @@ class Scars:
             This is important for ensuring that legitimate grain data is not detected as scarred data.
             Note that width here is vertical, as scars are thin, horizontal features.
         min_scar_length: int
-            An integer that restricts the algorithm to only mark scars taht are as long or longer than this length.
+            An integer that restricts the algorithm to only mark scars that are as long or longer than this length.
             This is important for ensuring that noise or legitimate but sharp datapoints do not get detected as scars.
             Note that length here is horizontal, as scars are thin, horizontal features.
         Returns
         -------
-        np.ndarray
-            Returns a copy of the input image with any detected scars removed.
+        marked: np.ndarray
+            Returns a 2-D image of the same shape as the data image, where each pixel's value represents
+            a metric for how strongly that pixel is considered to be a scar. Higher values mean more likely
+            to be a scar.
         """
 
         img = np.copy(img)
@@ -302,12 +379,22 @@ class Scars:
 
         return marked
 
-    def remove_marked_scars(self, img: np.ndarray, scar_mask: np.ndarray) -> np.ndarray:
+    def remove_marked_scars(self, img: np.ndarray, scar_mask: np.ndarray) -> None:
         """
         Interpolate values covered by marked scars. Takes an image, and a marked scar boolean mask for that
         image. Returns the image where the marked scars are replaced by interpolated values.
 
+        Parameters
+        ----------
+        img: np.ndarray
+            A 2-D image of the data to remove scars from.
+        scar_mask: np.ndarray
+            A boolean image of pixels that determine which values are flagged as scars and therefore should
+            be interpolated over in the original data image.
 
+        Returns
+        -------
+        None
         """
         for row in range(img.shape[0]):
             for col in range(img.shape[1]):
@@ -335,17 +422,18 @@ class Scars:
                         scar_mask[row + k - 1, col] = 0.0
                         k -= 1
 
-        return img
-
     def remove_scars(self):
         """
         Remove scars from an image.
         Scars are long, typically 1-4 pixels wide streaks of high or low data in AFM images. They are a problem
         resulting from random errors in the AFM data collection process and are hard to avoid. This function
         detects and removes these artefacts by interpolating over them between the pixels above and below them.
+        This method takes no parameters as it uses parameters already established as instance variables when the
+        class was instantiated.
 
         Parameters
         ----------
+        None
 
         Returns
         -------
@@ -377,7 +465,7 @@ class Scars:
                 )
                 # Combine the upper and lower scar masks
                 marked_both = np.bitwise_or(marked_positive.astype(bool), marked_negative.astype(bool))
-                self.img = self.remove_marked_scars(self.img, marked_both)
+                self.remove_marked_scars(self.img, marked_both)
         else:
             LOGGER.info(f"[{self.filename}] : Skipping scar removal as requested from config.")
         return self.img
