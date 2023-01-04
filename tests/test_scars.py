2ddecb6c0481b3fc9f92827969b60e6bbccbbc3d
SylviaWhittle <sylviwhittle@gmail.com>
2023-01-04 11:36:16 +0000

    Add regression test for the marked scars array

--- tests/test_scars.py
+++ tests/test_scars.py
@@ -11,8 +11,10 @@ RESOURCES = BASE_DIR / "tests" / "resources"
 def test_remove_scars(synthetic_scar_removal: Scars):
     """Test removal of scars"""
 
-    scars_removed, _scar_mask = synthetic_scar_removal.remove_scars()
+    scars_removed, scar_mask = synthetic_scar_removal.remove_scars()
 
     target_img = np.load(RESOURCES / "synthetic_scar_removal.npy")
+    target_mask = np.load(RESOURCES / "synthetic_scar_removal_mask.npy")
 
     np.testing.assert_array_equal(scars_removed, target_img)
+    np.testing.assert_array_equal(scar_mask, target_mask)
