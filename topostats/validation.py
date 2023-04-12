3b9135c7d2da9cd98bae1e38b482b67290da6b98
Max Gamill <mcgamill1@sheffield.ac.uk>
2023-04-12 13:42:47 +0100

    added avg crossings and zooms to validation

--- topostats/validation.py
+++ topostats/validation.py
@@ -686,7 +686,33 @@ PLOTTING_SCHEMA = Schema(
                 "core_set": bool,
                 "dpi": int,
             },
+            "zoom_node":{
+                "title": str,
+                "image_type": Or(
+                    "binary",
+                    "non-binary",
+                    error=(
+                        "Invalid value in config 'coloured_boxes.image_type', valid values "
+                        "are 'binary' or 'non-binary'"
+                    )
+                ),
+                "mask_cmap": str,
+                "core_set": bool,
+            },
             "crossings":{
+                "title": str,
+                "image_type": Or(
+                    "binary",
+                    "non-binary",
+                    error=(
+                        "Invalid value in config 'coloured_boxes.image_type', valid values "
+                        "are 'binary' or 'non-binary'"
+                    )
+                ),
+                "mask_cmap": str,
+                "core_set": bool,
+            },
+            "tripple_crossings":{
                 "title": str,
                 "image_type": Or(
                     "binary",
