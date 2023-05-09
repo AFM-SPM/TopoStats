e3aace70d69913fc98d29eb30b42a3d15e31444e
SylviaWhittle <sylviwhittle@gmail.com>
2023-05-09 19:55:27 +0100

    Add roughness calculation

---
+++ topostats/roughness.py
@@ -0,0 +1,17 @@
+"""Contains functions pertaining to the roughness of AFM images."""
+
+import numpy as np
+
+
+def roughness_rms(image: np.ndarray) -> float:
+    """Calculate the rms roughness of a heightmap image.
+    Parameters
+    ----------
+    image: np.ndarray
+        2D numpy array of heightmap data to calculate the roughness of
+
+    Returns:
+    float
+        The RMS roughness of the input array.
+    """
+    return np.sqrt(np.mean(np.square(image)))
