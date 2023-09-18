77ce3a60e707d008ab816087a4b9e6d7cc35b6e5
Laura Wiggins <l.wiggins@sheffield.ac.uk>
2023-09-18 10:43:34 +0100

    Changed colour of spline trace

--- topostats/plottingfuncs.py
+++ topostats/plottingfuncs.py
@@ -229,7 +229,7 @@ class Images:
                 plt.legend(handles=patch, loc="upper right", bbox_to_anchor=(1, 1.06))
             elif self.plot_coords is not None:
                 for grain_coords in self.plot_coords:
-                    plt.plot(grain_coords[:,1]*self.pixel_to_nm_scaling, (shape[1] - grain_coords[:,0])*self.pixel_to_nm_scaling , c='c', linewidth=0.5)
+                    plt.plot(grain_coords[:,1]*self.pixel_to_nm_scaling, (shape[1] - grain_coords[:,0])*self.pixel_to_nm_scaling , c='b', linewidth=1)
 
             plt.title(self.title)
             plt.xlabel("Nanometres")
