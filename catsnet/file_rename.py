741352a9a8173c201f723a61969f395356d2bba5
SylviaWhittle <sylviwhittle@gmail.com>
2023-11-10 10:19:51 +0000

    Add checks for the file rename script

--- catsnet/file_rename.py
+++ catsnet/file_rename.py
@@ -4,13 +4,22 @@ of the file as it appears in the directory"""
 from pathlib import Path
 import sys
 
+# Check that the correct number of arguments have been passed
+if len(sys.argv) != 3:
+    print("Usage: python file_rename.py <path> <file_extension>")
+    sys.exit()
+
 # Fetch the first argument as the path to the directory
 path = Path(sys.argv[1])
 if not path.exists():
     print("Path does not exist: ", path)
-    exit()
+    sys.exit()
 
+# Get the file extension from the second argument
 file_ext = sys.argv[2]
+# Ensure that the file extension starts with a period
+if file_ext[0] != ".":
+    file_ext = "." + file_ext
 
 i = 0
 
@@ -19,7 +28,7 @@ files.sort()
 
 if len(files) == 0:
     print("No files found")
-    exit()
+    sys.exit()
 else:
     for file in files:
         print(file.name)
