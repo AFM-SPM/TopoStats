284ec84261c324008438182eac3b481696c47dff
Neil Shephard <n.shephard@sheffield.ac.uk>
2023-12-05 16:59:05 +0000

    Add snoop as a dependency but disable by default
    I came across [snoop](https://github.com/alexmojaki/snoop) which provides a powerful set of debuggin tools and think it
    would be useful to use it in TopoStats.
    
    This PR adds it as a `dev` optional dependency and imports and installs during package initialisation. By default
    thought it is disabled so that performance in not impacted, this is done by `snoop.install(enabled=False)` in
    `__init__.py`.
    
    To enable and using during development add the `@snoop` decorator to a function, method or class and in the same file
    re-enable dynamically by calling `snoop.install(enabled=True)`.

--- topostats/__init__.py
+++ topostats/__init__.py
@@ -2,6 +2,7 @@
 from importlib.metadata import version
 
 import matplotlib.pyplot as plt
+import snoop
 
 from .logs.logs import setup_logger
 from .theme import Colormap
@@ -13,3 +14,6 @@ __version__ = ".".join(release.split("."[:2]))
 
 plt.register_cmap(cmap=Colormap("nanoscope").get_cmap())
 plt.register_cmap(cmap=Colormap("gwyddion").get_cmap())
+
+# Disable snoop
+snoop.install(enabled=False)
