df3e5a1d6554d783fdc8813c09f4743642cf8de9
SylviaWhittle <sylviwhittle@gmail.com>
2023-12-06 15:43:11 +0000

    Implement new unet model

--- topostats/grain_finding_cats_unet.py
+++ topostats/grain_finding_cats_unet.py
@@ -2,21 +2,26 @@
 
 
 from pathlib import Path
+import logging
 
-import os
 import numpy as np
 import tensorflow as tf
 from PIL import Image
 import matplotlib.pyplot as plt
 from skimage.feature import hessian_matrix, hessian_matrix_eigvals
 
+from topostats.logs.logs import LOGGER_NAME
+
+LOGGER = logging.getLogger(LOGGER_NAME)
+
 
 def test_GPU():
     """Ensure that the GPU is working correctly"""
     # for key, value in os.environ.items():
-    #     print(f"{key} : {value}")
+    #     LOGGER.info(f"{key} : {value}")
+    LOGGER.info("============= GPU TEST =============")
     tf.test.gpu_device_name()
-    print("GPU TEST OVER")
+    LOGGER.info("============= GPU TEST DONE =============")
 
 
 def normalise_image(image: np.ndarray) -> np.ndarray:
@@ -47,72 +52,57 @@ def predict_unet(
 ) -> np.ndarray:
     """Predict cats segmentation from a flattened image."""
 
+    # Make a copy of the original image
     original_image = image.copy()
 
-    # Scale the image to ensure there are no messy negatives etc
+    # Get the path to a file in the topostats package
+    model_path = Path(__file__).parent / "./catsnet_cropped_2023-11-10_17-45-07.h5"
+
+    LOGGER.info("Loading Unet model")
+    model = load_model(model_path=model_path)
+    LOGGER.info("Loaded Unet model")
+
+    # Run the model on a single image
+    LOGGER.info("Preprocessing image for Unet prediction...")
+
+    # Normalise the image
+    LOGGER.info("normalising image")
     image = image - np.min(image)
-    image = image / (np.max(image) / 255)
+    image = image / np.max(image)
 
-    # invert the image since it was trained with binary colour mapping which inverts it
-    # when loaded via cv2.
-    image = 255 - image
+    # Resize the image to 512x512
+    image = Image.fromarray(image)
+    image = image.resize((512, 512))
+    image = np.array(image)
 
-    # Detect ridges on the image
-    maxima, minima = detect_ridges(image.copy(), sigma=4.0)
+    # Predict the mask
+    LOGGER.info("Running Unet & predicting mask")
+    prediction = model.predict(np.expand_dims(image, axis=0))
+    LOGGER.info("Unet finished, predicted mask. saving...")
 
-    # Plot maxima and minima
-    # fig, ax = plt.subplots(1, 2)
-    # im1 = ax[0].imshow(maxima, cmap="binary")
-    # ax[0].set_title("maxima")
-    # im2 = ax[1].imshow(minima, cmap="binary")
-    # ax[1].set_title("minima")
-    # fig.colorbar(im1)
-    # fig.colorbar(im2)
-    # fig.savefig("./maxima_minima.png")
+    # Threshold the predicted mask
+    predicted_mask = prediction > confidence
+    # Remove the batch dimension
+    predicted_mask = predicted_mask[0, :, :, 0]
 
-    # Use maxima
-    ridges = maxima
+    # Resize the predicted mask back to the original image size
+    predicted_mask = Image.fromarray(predicted_mask)
+    predicted_mask = predicted_mask.resize(original_image.shape)
+    predicted_mask = np.array(predicted_mask)
 
-    to_predict = ridges.copy()
-
-    # Get the image into the correct format for prediction
-    to_predict = normalise_image(to_predict)
-    print(f"normalised image shape: {ridges.shape}")
-    # Resize image - this is really janky
-    to_predict = Image.fromarray(to_predict)
-    to_predict = to_predict.resize((model_image_size, model_image_size))
-    to_predict = np.array(to_predict)
-    to_predict = [to_predict]
-    to_predict = np.array(to_predict)
-    to_predict = np.expand_dims(to_predict, 3)
-    to_predict = to_predict[0]
-    to_predict = to_predict[:, :, 0][:, :, None]
-    to_predict = np.expand_dims(to_predict, 0)
-
-    # Predict segmentation
-    # model = load_model(model_path="./models/20230817_10-26-23_cats_512_b4_e50_hessian_upper_4_0.hdf5")
-    model = load_model(
-        model_path="/Users/Maxgamill/Desktop/Uni/PhD/topo_cats/TopoStats/topostats/20230817_10-26-23_cats_512_b4_e50_hessian_upper_4_0.hdf5"
-    )
-    prediction = (model.predict(to_predict)[0, :, :, 0] > confidence).astype(np.uint8)
-
+    # Save the predicted mask
     fig, ax = plt.subplots(1, 3, figsize=(20, 7))
-    ax[1].imshow(ridges)
-    ax[1].set_title("ridges")
+    ax[1].imshow(original_image)
+    ax[1].set_title("normalised image")
     ax[0].imshow(original_image)
     ax[0].set_title("original image")
-    ax[2].imshow(prediction)
-    ax[2].set_title("prediction")
+    ax[2].imshow(predicted_mask)
+    ax[2].set_title("predicted mask")
     fig.suptitle(f"strictness: {confidence}")
     fig.tight_layout()
-    # plt.savefig(image_output_dir / f"{filename}_prediction.png")
+    plt.savefig(image_output_dir / f"{filename}_prediction.png")
 
-    # Resize prediction back up to original size
-    prediction = Image.fromarray(prediction)
-    prediction = prediction.resize(original_image.shape)
-    prediction = np.array(prediction)
-
-    return prediction
+    return predicted_mask
 
 
 if __name__ == "__main__":
