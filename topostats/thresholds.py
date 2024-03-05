db46606b90aa899972409eef566443ed103cd20c
Neil Shephard <n.shephard@sheffield.ac.uk>
2024-03-05 16:34:26 +0000

    numpydoc : topostats/thresholding.py

--- topostats/thresholds.py
+++ topostats/thresholds.py
@@ -4,7 +4,7 @@
 import logging
 from collections.abc import Callable
 
-import numpy as np
+import numpy.typing as npt
 from skimage.filters import threshold_mean, threshold_minimum, threshold_otsu, threshold_triangle, threshold_yen
 
 from topostats.logs.logs import LOGGER_NAME
@@ -15,13 +15,18 @@ LOGGER = logging.getLogger(LOGGER_NAME)
 # pylint: disable=unused-argument
 
 
-def threshold(image: np.ndarray, method: str = None, otsu_threshold_multiplier: float = None, **kwargs: dict) -> float:
-    """Thresholding for producing masks.
+def threshold(image: npt.NDArray, method: str = None, otsu_threshold_multiplier: float = None, **kwargs: dict) -> float:
+    """
+    Thresholding for producing masks.
 
     Parameters
     ----------
+    image : npt.NDArray
+        2-D Numpy array of image for thresholding.
     method : str
         Method to use for thresholding, currently supported methods are otsu (default), mean and minimum.
+    otsu_threshold_multiplier : float
+        Factor for scaling the Otsu threshold.
     **kwargs : dict
         Additional keyword arguments to pass to skimage methods.
 
@@ -35,7 +40,8 @@ def threshold(image: np.ndarray, method: str = None, otsu_threshold_multiplier: 
 
 
 def _get_threshold(method: str = "otsu") -> Callable:
-    """Creator component which determines which threshold method to use.
+    """
+    Creator component which determines which threshold method to use.
 
     Parameters
     ----------
@@ -65,21 +71,121 @@ def _get_threshold(method: str = "otsu") -> Callable:
     raise ValueError(method)
 
 
-def _threshold_otsu(image: np.ndarray, otsu_threshold_multiplier: float = None, **kwargs) -> float:
+def _threshold_otsu(image: npt.NDArray, otsu_threshold_multiplier: float = None, **kwargs) -> float:
+    """
+    Calculate the Otsu threshold.
+
+    For more information see `skimage.filters.threshold_otsu()
+    <https://scikit-image.org/docs/stable/api/skimage.filters.html#skimage.filters.threshold_otsu>`_.
+
+    Parameters
+    ----------
+    image : npt.NDArray
+        2-D Numpy array of image for thresholding.
+    otsu_threshold_multiplier : float
+        Factor for scaling Otsu threshold.
+    **kwargs : dict
+        Dictionary of keyword arguments to pass to 'skimage.filters.threshold_otsu(**kwargs)'.
+
+    Returns
+    -------
+    float
+        Threshold to be used in masking heights.
+    """
     return threshold_otsu(image, **kwargs) * otsu_threshold_multiplier
 
 
-def _threshold_mean(image: np.ndarray, otsu_threshold_multiplier: float = None, **kwargs) -> float:
+def _threshold_mean(image: npt.NDArray, otsu_threshold_multiplier: float = None, **kwargs) -> float:
+    """
+    Calculate the Mean threshold.
+
+    For more information see `skimage.filters.threshold_mean()
+    <https://scikit-image.org/docs/stable/api/skimage.filters.html#skimage.filters.threshold_mean>`_.
+
+    Parameters
+    ----------
+    image : npt.NDArray
+        2-D Numpy array of image for thresholding.
+    otsu_threshold_multiplier : float
+        Factor for scaling (not used).
+    **kwargs : dict
+        Dictionary of keyword arguments to pass to 'skimage.filters.threshold_mean(**kwargs)'.
+
+    Returns
+    -------
+    float
+        Threshold to be used in masking heights.
+    """
     return threshold_mean(image, **kwargs)
 
 
-def _threshold_minimum(image: np.ndarray, otsu_threshold_multiplier: float = None, **kwargs) -> float:
+def _threshold_minimum(image: npt.NDArray, otsu_threshold_multiplier: float = None, **kwargs) -> float:
+    """
+    Calculate the Minimum threshold.
+
+    For more information see `skimage.filters.threshold_minimum()
+    <https://scikit-image.org/docs/stable/api/skimage.filters.html#skimage.filters.threshold_minimum>`_.
+
+    Parameters
+    ----------
+    image : npt.NDArray
+        2-D Numpy array of image for thresholding.
+    otsu_threshold_multiplier : float
+        Factor for scaling (not used).
+    **kwargs : dict
+        Dictionary of keyword arguments to pass to 'skimage.filters.threshold_minimum(**kwargs)'.
+
+    Returns
+    -------
+    float
+        Threshold to be used in masking heights.
+    """
     return threshold_minimum(image, **kwargs)
 
 
-def _threshold_yen(image: np.ndarray, otsu_threshold_multiplier: float = None, **kwargs) -> float:
+def _threshold_yen(image: npt.NDArray, otsu_threshold_multiplier: float = None, **kwargs) -> float:
+    """
+    Calculate the Yen threshold.
+
+    For more information see `skimage.filters.threshold_yen()
+    <https://scikit-image.org/docs/stable/api/skimage.filters.html#skimage.filters.threshold_yen>`_.
+
+    Parameters
+    ----------
+    image : npt.NDArray
+        2-D Numpy array of image for thresholding.
+    otsu_threshold_multiplier : float
+        Factor for scaling (not used).
+    **kwargs : dict
+        Dictionary of keyword arguments to pass to 'skimage.filters.threshold_yen(**kwargs)'.
+
+    Returns
+    -------
+    float
+        Threshold to be used in masking heights.
+    """
     return threshold_yen(image, **kwargs)
 
 
-def _threshold_triangle(image: np.ndarray, otsu_threshold_multiplier: float = None, **kwargs) -> float:
+def _threshold_triangle(image: npt.NDArray, otsu_threshold_multiplier: float = None, **kwargs) -> float:
+    """
+    Calculate the triangle threshold.
+
+    For more information see `skimage.filters.threshold_triangle()
+    <https://scikit-image.org/docs/stable/api/skimage.filters.html#skimage.filters.threshold_triangle>`_.
+
+    Parameters
+    ----------
+    image : npt.NDArray
+        2-D Numpy array of image for thresholding.
+    otsu_threshold_multiplier : float
+        Factor for scaling (not used).
+    **kwargs : dict
+        Dictionary of keyword arguments to pass to 'skimage.filters.threshold_triangle(**kwargs)'.
+
+    Returns
+    -------
+    float
+        Threshold to be used in masking heights.
+    """
     return threshold_triangle(image, **kwargs)
