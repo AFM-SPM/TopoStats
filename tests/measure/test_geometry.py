2fd66fc787de272988cbaac47cd59637e769e827
SylviaWhittle <sylviwhittle@gmail.com>
2024-07-02 21:23:46 +0100

    Add float and integer options for bounding box calculation

--- tests/measure/test_geometry.py
+++ tests/measure/test_geometry.py
@@ -4,13 +4,23 @@ import numpy as np
 import pytest
 from numpy.typing import NDArray
 
-from topostats.measure.geometry import bounding_box_cartesian_points, do_points_in_arrays_touch
+from topostats.measure.geometry import (
+    bounding_box_cartesian_points_float,
+    bounding_box_cartesian_points_integer,
+    do_points_in_arrays_touch,
+)
 
 
-def test_bounding_box_cartesian_points_raises_value_error():
+def test_bounding_box_cartesian_points_float_raises_value_error():
     """Test the bounding_box_cartesian_points function raises a ValueError."""
     with pytest.raises(ValueError, match="Input array must be Nx2."):
-        bounding_box_cartesian_points(np.array([[0, 0, 0], [1, 1, 1], [2, 2, 2]]))
+        bounding_box_cartesian_points_float(np.array([[0, 0, 0], [1, 1, 1], [2, 2, 2]]))
+
+
+def test_bounding_box_cartesian_points_integer_raises_value_error():
+    """Test the bounding_box_cartesian_points function raises a ValueError."""
+    with pytest.raises(ValueError, match="Input array must be Nx2."):
+        bounding_box_cartesian_points_float(np.array([[0, 0, 0], [1, 1, 1], [2, 2, 2]]))
 
 
 @pytest.mark.parametrize(
@@ -23,11 +33,28 @@ def test_bounding_box_cartesian_points_raises_value_error():
         pytest.param(np.array([[0.1, 0.1], [1.1, 1.1], [2.1, 2.1]]), (0.1, 0.1, 2.1, 2.1), id="diagonal line, floats"),
     ],
 )
-def test_bounding_box_cartesian_points(
+def test_bounding_box_cartesian_points_float(
     points: NDArray[np.number], expected_bbox: tuple[np.float64, np.float64, np.float64, np.float64]
 ):
     """Test the bounding_box_cartesian_points function."""
-    assert bounding_box_cartesian_points(points) == expected_bbox
+    assert bounding_box_cartesian_points_float(points) == expected_bbox
+
+
+@pytest.mark.parametrize(
+    ("points", "expected_bbox"),
+    [
+        pytest.param(np.array([[0, 0], [1, 1], [2, 2]]), (0, 0, 2, 2), id="diagonal line"),
+        pytest.param(np.array([[0, 0], [1, 1], [2, 0]]), (0, 0, 2, 1), id="triangle"),
+        pytest.param(np.array([[-5, -5], [-10, -10], [-3, -3]]), (-10, -10, -3, -3), id="negative values"),
+        pytest.param(np.array([[-1, -1], [1, 1], [2, 0], [-2, 0]]), (-2, -1, 2, 1), id="negative and positive values"),
+        pytest.param(np.array([[0.1, 0.1], [1.1, 1.1], [2.1, 2.1]]), (0, 0, 2, 2), id="diagonal line, floats"),
+    ],
+)
+def test_bounding_box_cartesian_points_integer(
+    points: NDArray[np.number], expected_bbox: tuple[np.float64, np.float64, np.float64, np.float64]
+):
+    """Test the bounding_box_cartesian_points function."""
+    assert bounding_box_cartesian_points_integer(points) == expected_bbox
 
 
 def test_do_points_in_arrays_touch_raises_value_error():
@@ -37,19 +64,44 @@ def test_do_points_in_arrays_touch_raises_value_error():
 
 
 @pytest.mark.parametrize(
-    ("array_1", "array_2", "expected"),
+    ("array_1", "array_2", "expected_result_touching", "expected_point_1_touching", "expected_point_2_touching"),
     [
         pytest.param(
-            np.array([[0, 0], [1, 1], [2, 2]]), np.array([[4, 4], [5, 5], [6, 6]]), False, id="no touching points"
+            np.array([[0, 0], [1, 1], [2, 2]]),
+            np.array([[4, 4], [5, 5], [6, 6]]),
+            False,
+            None,
+            None,
+            id="no touching points",
         ),
         pytest.param(
-            np.array([[0, 0], [1, 1], [2, 2]]), np.array([[2, 3], [3, 4], [5, 5]]), True, id="touching points, non_diag"
+            np.array([[0, 0], [1, 1], [2, 2]]),
+            np.array([[2, 3], [3, 4], [5, 5]]),
+            True,
+            np.array([2, 2]),
+            np.array([2, 3]),
+            id="touching points, non_diag",
         ),
         pytest.param(
-            np.array([[0, 0], [1, 1], [2, 2]]), np.array([[3, 3], [4, 4], [5, 5]]), True, id="touching points, diag"
+            np.array([[0, 0], [1, 1], [2, 2]]),
+            np.array([[3, 3], [4, 4], [5, 5]]),
+            True,
+            np.array([2, 2]),
+            np.array([3, 3]),
+            id="touching points, diag",
         ),
     ],
 )
-def test_do_points_in_arrays_touch(array_1: NDArray[np.number], array_2: NDArray[np.number], expected: bool):
+def test_do_points_in_arrays_touch(
+    array_1: NDArray[np.number],
+    array_2: NDArray[np.number],
+    expected_result_touching: bool,
+    expected_point_1_touching: NDArray[np.number],
+    expected_point_2_touching: NDArray[np.number],
+):
     """Test the do_points_in_arrays_touch function."""
-    assert do_points_in_arrays_touch(array_1, array_2) == expected
+    result_touching, point_touching_1, point_touching_2 = do_points_in_arrays_touch(array_1, array_2)
+
+    assert result_touching == expected_result_touching
+    np.testing.assert_array_equal(point_touching_1, expected_point_1_touching)
+    np.testing.assert_array_equal(point_touching_2, expected_point_2_touching)
--- topostats/measure/geometry.py
+++ topostats/measure/geometry.py
@@ -4,7 +4,9 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-def bounding_box_cartesian_points(points: NDArray[np.number]) -> tuple[np.float64, np.float64, np.float64, np.float64]:
+def bounding_box_cartesian_points_float(
+    points: NDArray[np.number],
+) -> tuple[np.float64, np.float64, np.float64, np.float64]:
     """
     Calculate the bounding box from a set of points.
 
@@ -29,7 +31,34 @@ def bounding_box_cartesian_points(points: NDArray[np.number]) -> tuple[np.float6
     return (np.min(x_coords), np.min(y_coords), np.max(x_coords), np.max(y_coords))
 
 
-def do_points_in_arrays_touch(points1: NDArray[np.number], points2: NDArray[np.number]) -> bool:
+def bounding_box_cartesian_points_integer(points: NDArray[np.number]) -> tuple[np.int32, np.int32, np.int32, np.int32]:
+    """
+    Calculate the bounding box from a set of points.
+
+    Parameters
+    ----------
+    points : NDArray[np.number]
+        Nx2 numpy array of points.
+
+    Returns
+    -------
+    Tuple[np.int32, np.int32, np.int32, np.int32]
+        Tuple of (min_x, min_y, max_x, max_y).
+
+    Raises
+    ------
+    ValueError
+        If the input array is not Nx2.
+    """
+    if points.shape[1] != 2:
+        raise ValueError("Input array must be Nx2.")
+    x_coords, y_coords = points[:, 0].astype(np.int32), points[:, 1].astype(np.int32)
+    return (np.min(x_coords), np.min(y_coords), np.max(x_coords), np.max(y_coords))
+
+
+def do_points_in_arrays_touch(
+    points1: NDArray[np.number], points2: NDArray[np.number]
+) -> tuple[bool, NDArray[np.number] | None, NDArray[np.number] | None]:
     """
     Check if any points in two arrays are touching.
 
@@ -42,8 +71,8 @@ def do_points_in_arrays_touch(points1: NDArray[np.number], points2: NDArray[np.n
 
     Returns
     -------
-    bool
-        True if any points in the two arrays are touching, False otherwise.
+    tuple[bool, NDArray[np.number] | None, NDArray[np.number] | None]
+        True if any points in the two arrays are touching, False otherwise, followed by the two points that touch.
 
     Raises
     ------
@@ -57,5 +86,5 @@ def do_points_in_arrays_touch(points1: NDArray[np.number], points2: NDArray[np.n
         for point2 in points2:
             diff = np.abs(point1 - point2)
             if np.all(diff <= 1):
-                return True
-    return False
+                return (True, point1, point2)
+    return (False, None, None)
