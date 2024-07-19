117a2a149fc397aa82913f1819dd73e7bc248b2a
Max Gamill <mcgamill1@sheffield.ac.uk>
2024-07-19 16:15:36 +0100

    Smoke renamed to regression + used conftest config file

--- tests/test_nodestats_regression.py
+++ tests/test_nodestats_regression.py
@@ -1,4 +1,4 @@
-"""Smoke test."""
+"""Regression test for nodestats tracing."""
 
 import importlib.resources as pkg_resources
 from pathlib import Path
@@ -14,26 +14,24 @@ from topostats.processing import (
 )
 
 BASE_DIR = Path.cwd()
-SMOKE = BASE_DIR / "tests/smoke"
+REGRESSION = BASE_DIR / "tests/nodestats-regression"
 
 
-def test_smoke(regtest, tmp_path, load_scan_spm: LoadScans) -> None:
+def test_nodestats_tracing_results(default_config, regtest, tmp_path, load_scan_spm: LoadScans) -> None:
     """Regression test for checking the process_scan functions correctly."""
     load_scan_spm.get_data()
     img_dic = load_scan_spm.img_dict
     # Ensure there are below grains
-    process_scan_config = read_yaml(BASE_DIR / "topostats" / "default_config.yaml")
-    plotting_dictionary = pkg_resources.open_text(topostats, "plotting_dictionary.yaml")
-    process_scan_config["plotting"]["plot_dict"] = yaml.safe_load(plotting_dictionary.read())
-    process_scan_config["grains"]["direction"] = "above"
+    default_config["grains"]["direction"] = "above"
     _, results, img_stats = process_scan(
         topostats_object=img_dic["minicircle"],
         base_dir=BASE_DIR,
-        filter_config=process_scan_config["filter"],
-        grains_config=process_scan_config["grains"],
-        grainstats_config=process_scan_config["grainstats"],
-        dnatracing_config=process_scan_config["dnatracing"],
-        plotting_config=process_scan_config["plotting"],
+        filter_config=default_config["filter"],
+        grains_config=default_config["grains"],
+        grainstats_config=default_config["grainstats"],
+        disordered_tracing_config=default_config["disordered_tracing"],
+        dnatracing_config=default_config["dnatracing"],
+        plotting_config=default_config["plotting"],
         output_dir=tmp_path,
     )
     # Remove the basename column as this differs on CI
@@ -46,7 +44,7 @@ def test_smoke(regtest, tmp_path, load_scan_spm: LoadScans) -> None:
     with h5py.File(tmp_path / "tests/resources/processed/minicircle.topostats", "r") as f:
         saved_topostats = hdf5_to_dict(f, group_path="/")
 
-    with h5py.File(SMOKE / "minicircle.topostats.out", "r") as f:
+    with h5py.File(REGRESSION / "minicircle.topostats.out", "r") as f:
         expected_topostats = hdf5_to_dict(f, group_path="/")
 
     # Remove the image path as this differs on CI
