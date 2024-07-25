344ebe42800558bf04333ca061eea586b85b0040
Max Gamill <mcgamill1@sheffield.ac.uk>
2024-07-25 16:37:13 +0100

    Add crossing linetrace plotting func

--- topostats/plotting.py
+++ topostats/plotting.py
@@ -10,6 +10,7 @@ from pathlib import Path
 import sys
 import yaml
 import matplotlib.pyplot as plt
+import matplotlib.colors
 import pandas as pd
 import seaborn as sns
 import numpy as np
@@ -17,6 +18,7 @@ import numpy as np
 from topostats.io import read_yaml, save_pkl, write_yaml, convert_basename_to_relative_paths
 from topostats.logs.logs import LOGGER_NAME
 from topostats.utils import update_config
+from topostats.theme import Colormap
 
 LOGGER = logging.getLogger(LOGGER_NAME)
 
@@ -473,3 +475,44 @@ def run_toposum(args=None) -> None:
 
     # Plot statistics
     toposum(config)
+
+
+def plot_crossing_linetrace_halfmax(branch_stats_dict: dict, cmap: matplotlib.colors.Colormap, title: str) -> tuple:
+    """Plots the heightmap lines traces of the branches found in the 'branch_stats' dictionary, and their meetings.
+
+    Parameters:
+    -----------
+    branch_stats_dict: dict
+        Dictionary containing branch height, distance and fwhm2 info.
+    """
+    fig, ax = plt.subplots(1, 1, figsize=(7, 4))
+    cmp = Colormap(cmap).get_cmap()
+    total_branches = len(branch_stats_dict)
+    # plot the highest first
+    fwhms = []
+    for branch_idx, values in branch_stats_dict.items():
+        fwhms.append(values["fwhm2"][0])
+    branch_idx_order = np.array(list(branch_stats_dict.keys()))[np.argsort(np.array(fwhms))]
+
+    for i, branch_idx in enumerate(branch_idx_order):
+        fwhm, hm_vals, m_vals = branch_stats_dict[branch_idx]["fwhm2"]
+        if total_branches == 1:
+            cmap_ratio = 0
+        else:
+            cmap_ratio = i / (total_branches - 1)
+        heights = branch_stats_dict[branch_idx]["heights"]
+        x = branch_stats_dict[branch_idx]["distances"]
+        ax.plot(x, heights, c=cmp(cmap_ratio)) # label=f"Branch: {branch_idx}"
+
+        # plot the high point lines
+        plt.plot([-15, m_vals[1]], [m_vals[2], m_vals[2]], c=cmp(cmap_ratio), label=f"FWHM: {fwhm:.4f}")
+        # plot the half max lines
+        plt.plot([hm_vals[0], hm_vals[0]], [hm_vals[2], heights.min()], c=cmp(cmap_ratio))
+        plt.plot([hm_vals[1], hm_vals[1]], [hm_vals[2], heights.min()], c=cmp(cmap_ratio))
+
+    ax.tick_params(axis='both', labelsize=20)
+    #ax.set_xlabel("Distance from Node (nm)", fontsize="22")
+    #ax.set_ylabel("Height", fontsize="22")
+    #ax.set_title(title, fontsize="20")
+    ax.legend(fontsize="16")
+    return fig, ax
