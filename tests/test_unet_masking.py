42770d5a423d76db45f5d2182ab5c95e66441c2b
SylviaWhittle <sylviwhittle@gmail.com>
2024-08-21 11:12:16 +0100

    Add test for pad_bounding_box

--- tests/test_unet_masking.py
+++ tests/test_unet_masking.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from topostats.unet_masking import make_crop_square
+from topostats.unet_masking import make_crop_square, pad_bounding_box
 
 
 @pytest.mark.parametrize(
@@ -23,7 +23,40 @@ from topostats.unet_masking import make_crop_square
         pytest.param(6, 1, 8, 7, (10, 10), (2, 1, 8, 7), id="constrained bottom"),
     ],
 )
-def test_make_crop_square(crop_min_row, crop_min_col, crop_max_row, crop_max_col, image_shape, expected_indices):
+def test_make_crop_square(
+    crop_min_row: int,
+    crop_min_col: int,
+    crop_max_row: int,
+    crop_max_col: int,
+    image_shape: tuple[int, int],
+    expected_indices: tuple[int, int, int, int],
+):
     """Test the crop square method."""
     result = make_crop_square(crop_min_row, crop_min_col, crop_max_row, crop_max_col, image_shape)
+    assert result == expected_indices
+
+
+@pytest.mark.parametrize(
+    ("crop_min_row", "crop_min_col", "crop_max_row", "crop_max_col", "image_shape", "padding", "expected_indices"),
+    [
+        pytest.param(2, 2, 6, 6, (10, 10), 0, (2, 2, 6, 6), id="no padding"),
+        pytest.param(0, 0, 10, 10, (10, 10), 5, (0, 0, 10, 10), id="crop too big to be padded"),
+        pytest.param(2, 2, 6, 6, (10, 10), 2, (0, 0, 8, 8), id="padding 2, no obstruction"),
+        pytest.param(0, 4, 6, 8, (10, 10), 2, (0, 2, 8, 10), id="padding 2, obstruction top"),
+        pytest.param(4, 0, 8, 6, (10, 10), 2, (2, 0, 10, 8), id="padding 2, obstruction left"),
+        pytest.param(4, 4, 10, 6, (10, 10), 2, (2, 2, 10, 8), id="padding 2, obstruction bottom"),
+        pytest.param(4, 4, 6, 10, (10, 10), 2, (2, 2, 8, 10), id="padding 2, obstruction right"),
+    ],
+)
+def test_pad_bounding_box(
+    crop_min_row: int,
+    crop_min_col: int,
+    crop_max_row: int,
+    crop_max_col: int,
+    image_shape: tuple[int, int],
+    padding,
+    expected_indices,
+):
+    """Test the pad bounding box method."""
+    result = pad_bounding_box(crop_min_row, crop_min_col, crop_max_row, crop_max_col, image_shape, padding)
     assert result == expected_indices
