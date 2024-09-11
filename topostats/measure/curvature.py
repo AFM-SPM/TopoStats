d72d0534035d9d97d4de1f4d927d6611e8b781cb
SylviaWhittle <sylviwhittle@gmail.com>
2024-09-11 15:45:28 +0100

    Add beginnings of curvature file

---
+++ topostats/measure/curvature.py
@@ -0,0 +1,9 @@
+"""Calculate various curvature metrics for traces."""
+
+from __future__ import annotations
+
+import logging
+
+from topostats.logs.logs import LOGGER_NAME
+
+LOGGER = logging.getLogger(LOGGER_NAME)
