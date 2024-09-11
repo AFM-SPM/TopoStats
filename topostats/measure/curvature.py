541a514fe9e9f6f4819589bf403dd9051b2e3297
SylviaWhittle <sylviwhittle@gmail.com>
2024-09-11 16:22:26 +0100

    Add angle_diff_signed

--- topostats/measure/curvature.py
+++ topostats/measure/curvature.py
@@ -4,6 +4,39 @@ from __future__ import annotations
 
 import logging
 
+import numpy as np
+import numpy.typing as npt
+
 from topostats.logs.logs import LOGGER_NAME
 
 LOGGER = logging.getLogger(LOGGER_NAME)
+
+
+def angle_diff_signed(v1: npt.NDArray[np.number], v2: npt.NDArray[np.number]):
+    """
+    Calculate the signed angle difference between two point vecrtors in 2D space.
+
+    Positive angles are clockwise, negative angles are counterclockwise.
+
+    Parameters
+    ----------
+    v1 : npt.NDArray[np.number]
+        First vector.
+    v2 : npt.NDArray[np.number]
+        Second vector.
+
+    Returns
+    -------
+    float
+        The signed angle difference in radians.
+    """
+    if v1.shape != (2,) or v2.shape != (2,):
+        raise ValueError("Vectors must be of shape (2,)")
+
+    angle = np.arctan2(v2[1], v2[0]) - np.arctan2(v1[1], v1[0])
+    if angle > np.pi:
+        angle -= 2 * np.pi
+    elif angle < -np.pi:
+        angle += 2 * np.pi
+
+    return angle
