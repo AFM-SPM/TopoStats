7fa02ef8ae0682d04fd70472883b8e73883b2a2c
SylviaWhittle <sylviwhittle@gmail.com>
2024-10-01 22:40:43 +0100

    Add test for discrete_angle_difference_per_nm_circular

--- tests/measure/test_curvature.py
+++ tests/measure/test_curvature.py
@@ -4,7 +4,7 @@ import numpy as np
 import numpy.typing as npt
 import pytest
 
-from topostats.measure.curvature import angle_diff_signed
+from topostats.measure.curvature import angle_diff_signed, discrete_angle_difference_per_nm_circular
 
 
 @pytest.mark.parametrize(
@@ -14,6 +14,10 @@ from topostats.measure.curvature import angle_diff_signed
         pytest.param(np.array([1, 0]), np.array([1, 0]), 0, id="same vectors"),
         pytest.param(np.array([1, 0]), np.array([0, 1]), np.pi / 2, id="up & right 90 deg"),
         pytest.param(np.array([0, 1]), np.array([1, 0]), -np.pi / 2, id="right & up -90 deg"),
+        pytest.param(np.array([-1, 0]), np.array([0, 1]), np.pi / 2, id="down & right 90 deg"),
+        pytest.param(np.array([0, -1]), np.array([-1, 0]), -np.pi / 2, id="left & down -90 deg"),
+        pytest.param(np.array([1, 0]), np.array([0, -1]), -np.pi / 2, id="up & left -90 deg"),
+        pytest.param(np.array([0, 1]), np.array([-1, 0]), np.pi / 2, id="left & up 90 deg"),
         pytest.param(np.array([2, 0]), np.array([0, 2]), np.pi / 2, id="up & right 90 deg, non-normalized"),
         pytest.param(np.array([-1, -1]), np.array([0, -1]), np.pi / 4, id="down & left -45 deg"),
         pytest.param(np.array([-1, -1]), np.array([1, 1]), np.pi, id="down-left & up-right 180 deg"),
@@ -22,3 +26,77 @@ from topostats.measure.curvature import angle_diff_signed
 def test_angle_diff_signed(v1: npt.NDArray[np.number], v2: npt.NDArray[np.number], expected_angle: float) -> None:
     """Test the signed angle difference calculation."""
     assert angle_diff_signed(v1, v2) == expected_angle
+
+
+@pytest.mark.parametrize(
+    ("trace_nm", "expected_angle_difference_per_nm"),
+    [
+        pytest.param(
+            np.array(
+                [
+                    [-1, -1],
+                    [-1, 1],
+                    [1, 1],
+                    [1, -1],
+                ]
+            ),
+            np.array(
+                [
+                    -np.pi / 4,
+                    -np.pi / 4,
+                    -np.pi / 4,
+                    -np.pi / 4,
+                ]
+            ),
+            id="square counter-clockwise",
+        ),
+        pytest.param(
+            np.array(
+                [
+                    [-1, -1],
+                    [1, -1],
+                    [1, 1],
+                    [-1, 1],
+                ]
+            ),
+            np.array(
+                [
+                    np.pi / 4,
+                    np.pi / 4,
+                    np.pi / 4,
+                    np.pi / 4,
+                ]
+            ),
+            id="square clockwise",
+        ),
+        pytest.param(
+            np.array(
+                [
+                    [-2, -2],
+                    [2, -2],
+                    [2, 2],
+                    [-2, 2],
+                ]
+            ),
+            np.array(
+                [
+                    np.pi / 8,
+                    np.pi / 8,
+                    np.pi / 8,
+                    np.pi / 8,
+                ]
+            ),
+            id="2 wide square clockwise",
+        ),
+    ],
+)
+def test_discrete_angle_difference_per_nm_circular(
+    trace_nm: npt.NDArray[np.number], expected_angle_difference_per_nm: npt.NDArray[np.number]
+) -> None:
+    """Test the discrete angle difference per nm calculation."""
+    # Calculate the angle difference per nm
+    angle_difference_per_nm = discrete_angle_difference_per_nm_circular(
+        trace_nm=trace_nm,
+    )
+
+    np.testing.assert_array_equal(angle_difference_per_nm, expected_angle_difference_per_nm)
