55d0e7cb7aca2b7c2d6cd729292a0c40b0e93eae
Max Gamill <91465918+MaxGamill-Sheffield@users.noreply.github.com>
2024-10-10 20:39:27 +0000

    From refs/pull/932/merge 3489a8a90cfead3a43f325233b469422ff6fa49d

---
+++ main/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2.2.2.dev823+g1e8f1a96',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
