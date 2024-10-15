16499f03247893003d2d8ecc0fd04dc4ee89676a
Neil Shephard <n.shephard@sheffield.ac.uk>
2024-10-15 11:10:51 +0100

    style(test/tracing): pass > pytest.mark.skip() for missing tests
    Means we can remove `# pylint: disable=unnecessary-pass` from these files and we get a reminder of what tests still need
    writing.

--- tests/tracing/test_nodestats.py
+++ tests/tracing/test_nodestats.py
@@ -21,56 +21,53 @@ DISORDERED_TRACING_RESOURCES = GENERAL_RESOURCES / "tracing" / "disordered_traci
 NODESTATS_RESOURCES = GENERAL_RESOURCES / "tracing" / "nodestats"
 # from topostats.tracing.nodestats import nodeStats
 
-# pylint: disable=unnecessary-pass
 # pylint: disable=too-many-arguments
 # pylint: disable=too-many-locals
 # pylint: disable=too-many-lines
 
 
-# @pytest.mark.parametrize()
 def test_get_node_stats() -> None:
     """Test of get_node_stats() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_check_node_errorless() -> None:
     """Test of check_node_errorless() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_skeleton_image_to_graph() -> None:
     """Test of skeleton_image_to_graph() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_graph_to_skeleton_image() -> None:
     """Test of graph_to_skeleton_image() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_tidy_branches() -> None:
     """Test of tidy_branches() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_keep_biggest_object() -> None:
     """Test of keep_biggest_object() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_connect_close_nodes() -> None:
     """Test of connect_close_nodes() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_highlight_node_centres() -> None:
     """Test of highlight_node_centres() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_connect_extended_nodes() -> None:
     """Test of connect_extended_nodes() method of nodeStats class."""
-    pass
 
 
 @pytest.mark.parametrize(
@@ -182,9 +179,9 @@ def test_connect_extended_nodes_nearest(
     assert np.array_equal(result, expected_nodes)
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_find_branch_starts() -> None:
     """Test of find_branch_starts() method of nodeStats class."""
-    pass
 
 
 # Create nodestats class using the cats image - will allow running the code for diagnostics
@@ -600,157 +597,156 @@ def test_get_ordered_branches_and_vectors(
 
 def test_sq() -> None:
     """Test of sq() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_tri() -> None:
     """Test of tri() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_auc() -> None:
     """Test of auc() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_cross_confidence() -> None:
     """Test of cross_confidence() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_recip() -> None:
     """Test of recip() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_per_diff() -> None:
     """Test of per_diff() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_detect_ridges() -> None:
     """Test of detect_ridges() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_get_box_lims() -> None:
     """Test of get_box_lims() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_order_branch() -> None:
     """Test of order_branch() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_order_branch_from_start() -> None:
     """Test of order_branch_from_start() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_local_area_sum() -> None:
     """Test of local_area_sum() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_get_vector() -> None:
     """Test of get_vector() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_calc_angles() -> None:
     """Test of calc_angles() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_pair_vectors() -> None:
     """Test of pair_vectors() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_best_matches() -> None:
     """Test of best_matches() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_create_weighted_graph() -> None:
     """Test of create_weighted_graph() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_pair_angles() -> None:
     """Test of pair_angles() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_gaussian() -> None:
     """Test of gaussian() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_fwhm() -> None:
     """Test of fwhm() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_fwhm2() -> None:
     """Test of fwhm2() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_peak_height() -> None:
     """Test of peak_height() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_lin_interp() -> None:
     """Test of lin_interp() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_close_coords() -> None:
     """Test of close_coords() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_order_branches() -> None:
     """Test of order_branches() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_binary_line() -> None:
     """Test of binary_line() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_coord_dist() -> None:
     """Test of coord_dist() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_coord_dist_rad() -> None:
     """Test of coord_dist_rad() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_above_below_value_idx() -> None:
     """Test of above_below_value_idx() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_average_height_trace() -> None:
     """Test of average_height_trace() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_fill_holes() -> None:
     """Test of fill_holes() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_remove_re_entering_branches() -> None:
     """Test of remove_re_entering_branches() method of nodeStats class."""
-    pass
 
 
 @pytest.mark.parametrize(
@@ -808,72 +804,71 @@ def test_only_centre_branches(
 
 def test_average_uniques() -> None:
     """Test of average_uniques() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_compile_trace() -> None:
     """Test of compile_trace() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_get_minus_img() -> None:
     """Test of get_minus_img() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_remove_common_values() -> None:
     """Test of remove_common_values() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_trace() -> None:
     """Test of trace() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_reduce_rows() -> None:
     """Test of reduce_rows() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_get_trace_segment() -> None:
     """Test of get_trace_segment() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_comb_xyzs() -> None:
     """Test of comb_xyzs() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_remove_duplicates() -> None:
     """Test of remove_duplicates() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_order_from_end() -> None:
     """Test of order_from_end() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_get_trace_idxs() -> None:
     """Test of get_trace_idxs() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_get_visual_img() -> None:
     """Test of get_visual_img() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_average_crossing_confs() -> None:
     """Test of average_crossing_confs() method of nodeStats class."""
-    pass
 
 
+@pytest.mark.skip(reason="Awaiting test to be written 2024-10-15")
 def test_minimum_crossing_confs() -> None:
     """Test minimum_crossing_confs() method of nodeStats class."""
-    pass
 
 
 @pytest.mark.parametrize(
