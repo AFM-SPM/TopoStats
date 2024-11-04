e9287fcb0d58e915a136a3ea40e4ea4741f7e3fc
Jean <mdu12@sheffield.ac.uk>
2024-11-04 17:55:56 +0000

    Simplify jaccardindex.py

--- jaccardindex.py
+++ jaccardindex.py
@@ -1,24 +1,30 @@
 from PIL import Image
 import configparser
 import numpy as np
+import os
 
-# Read .tiff files of masks
+# Read .tiff or .npy files of masks
 config = configparser.ConfigParser()
 config.read("config/config.ini")
 imagepath1 = config.get("MainSection", "imagepath1")
 imagepath2 = config.get("MainSection", "imagepath2")
-image_manual = Image.open(imagepath1)
-array_automatic = np.load(imagepath2).astype(int)
 
-# Convert images to numpy arrays
-array_manual = np.array(image_manual)[:, :, 0]
-array_automatic = np.transpose(array_automatic)
-# Convert arrays to binary
-array_manual[array_manual != 0] = 1
-array_automatic[array_automatic != 0] = 1
+# Convert data into numpy array
+arraylist = []
+for maskpath in [imagepath1, imagepath2]:
+    if os.path.splitext(maskpath)[1] == '.tiff':
+        maskimage = Image.open(maskpath)
+        maskarray = np.array(maskimage)[:, :, 0]
+        maskarray[maskarray != 0] = 1  # Convert array into binary
+        arraylist.append(maskarray)
+    elif os.path.splitext(maskpath)[1] == '.npy':
+        maskarray = np.load(maskpath).astype(int)
+        maskarray = np.transpose(maskarray)
+        maskarray[maskarray != 0] = 1  # Convert array into binary
+        arraylist.append(maskarray)
 
 # Calculate the Jaccard index
-array_sum = array_manual + array_automatic
+array_sum = arraylist[0] + arraylist[1]
 list_sum = list(array_sum.flatten())
 overlap = list_sum.count(2)  # Number of pixels masked in both images
 total = list_sum.count(1) + list_sum.count(2)  # Number of pixels masked in at least one image
