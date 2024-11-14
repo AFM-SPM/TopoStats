d81f92cfa7cbadf81c9d62ae7312d8b03bef7d68
TomC <t.catley@sheffield.ac.uk>
2024-11-14 16:01:52 +0000

    added dna width measurement in disordered tracing

--- topostats/tracing/disordered_tracing.py
+++ topostats/tracing/disordered_tracing.py
@@ -18,6 +18,7 @@ from topostats.logs.logs import LOGGER_NAME
 from topostats.tracing.pruning import prune_skeleton
 from topostats.tracing.skeletonize import getSkeleton
 from topostats.utils import convolve_skeleton
+from scipy.ndimage import distance_transform_edt
 
 LOGGER = logging.getLogger(LOGGER_NAME)
 
@@ -130,8 +131,6 @@ class disorderedTrace:  # pylint: disable=too-many-instance-attributes
         )
         self.pruned_skeleton = self.remove_touching_edge(self.pruned_skeleton)
         self.disordered_trace = np.argwhere(self.pruned_skeleton == 1)
-
-        disorderedTrace.calculate_dna_width(trace=self.disordered_trace, mask=self.smoothed_mask)
 
         if self.disordered_trace is None:
             LOGGER.warning(f"[{self.filename}] : Grain {self.n_grain} failed to Skeletonise.")
@@ -257,18 +256,16 @@ class disorderedTrace:  # pylint: disable=too-many-instance-attributes
         return self.re_add_holes(grain, dilation, holearea_min_max)
     
     @staticmethod
-    def calculate_dna_width(trace: npt.NDArray, mask: npt.NDArray) -> float:
+    def calculate_dna_width(smoothed_mask: npt.NDArray, pruned_skeleton: npt.NDArray, px2nm: float = 1) -> float:
         """
         Calculate the average width in metres of the DNA using the trace and mask.
         """
 
-        import matplotlib.pyplot as plt
-        from scipy.ndimage import distance_transform_edt
         # Code will go here
+        dist_trans = distance_transform_edt(smoothed_mask)
+        comb = np.where(pruned_skeleton==1,dist_trans,0)
 
-        average_dna_width = 5
-
-        return average_dna_width * 1e-9
+        return comb[comb!=0].mean() * 2 * px2nm
 
 
 def trace_image_disordered(  # pylint: disable=too-many-arguments,too-many-locals
@@ -379,6 +376,7 @@ def trace_image_disordered(  # pylint: disable=too-many-arguments,too-many-local
                     "grain_endpoints": np.int64((conv_pruned_skeleton == 2).sum()),
                     "grain_junctions": np.int64((conv_pruned_skeleton == 3).sum()),
                     "total_branch_lengths": total_branch_length,
+                    "grain_width": disorderedTrace.calculate_dna_width(disordered_trace_images["smoothed_grain"], disordered_trace_images["pruned_skeleton"], pixel_to_nm_scaling) * 1e-9
                 }
 
                 # remap the cropped images back onto the original
