6e3551e0c3aaa529a57dc8756caf61b57f74382d
Neil Shephard <n.shephard@sheffield.ac.uk>
2024-12-02 13:21:25 +0000

    refactor(io): Load .gwy with AFMReader

--- topostats/io.py
+++ topostats/io.py
@@ -19,7 +19,7 @@ import numpy as np
 import numpy.typing as npt
 import pandas as pd
 import pySPM
-from AFMReader import asd, ibw, jpk, spm, topostats
+from AFMReader import asd, ibw, gwy, jpk, spm, topostats
 from numpyencoder import NumpyEncoder
 from ruamel.yaml import YAML, YAMLError
 
@@ -747,157 +747,6 @@ class LoadScans:
             LOGGER.error(f"[{self.filename}] File not found : {self.img_path}")
             raise
 
-    @staticmethod
-    def _gwy_read_object(open_file: io.TextIOWrapper, data_dict: dict) -> None:
-        """
-        Parse and extract data from a `.gwy` file object, starting at the current open file read position.
-
-        Parameters
-        ----------
-        open_file : io.TextIOWrapper
-            An open file object.
-        data_dict : dict
-            Dictionary of `.gwy` file image properties.
-        """
-        object_name = read_null_terminated_string(open_file=open_file)
-        data_size = read_u32i(open_file)
-        LOGGER.debug(f"OBJECT | name: {object_name} | data_size: {data_size}")
-        # Read components
-        read_data_size = 0
-        while read_data_size < data_size:
-            component_data_size = LoadScans._gwy_read_component(
-                open_file=open_file,
-                initial_byte_pos=open_file.tell(),
-                data_dict=data_dict,
-            )
-            read_data_size += component_data_size
-
-    @staticmethod
-    def _gwy_read_component(open_file: io.TextIOWrapper, initial_byte_pos: int, data_dict: dict) -> int:
-        """
-        Parse and extract data from a `.gwy` file object, starting at the current open file read position.
-
-        Parameters
-        ----------
-        open_file : io.TextIOWrapper
-            An open file object.
-        initial_byte_pos : int
-            Initial position, as byte.
-        data_dict : dict
-            Dictionary of `.gwy` file image properties.
-
-        Returns
-        -------
-        int
-            Size of the component in bytes.
-        """
-        component_name = read_null_terminated_string(open_file=open_file)
-        data_type = read_gwy_component_dtype(open_file=open_file)
-
-        if data_type == "o":
-            LOGGER.debug(f"component name: {component_name} | dtype: {data_type} |")
-            sub_dict = {}
-            LoadScans._gwy_read_object(open_file=open_file, data_dict=sub_dict)
-            data_dict[component_name] = sub_dict
-        elif data_type == "c":
-            value = read_char(open_file=open_file)
-            LOGGER.debug(f"component name: {component_name} | dtype: {data_type} | value: {value}")
-            data_dict[component_name] = value
-        elif data_type == "i":
-            value = read_u32i(open_file=open_file)
-            LOGGER.debug(f"component name: {component_name} | dtype: {data_type} | value: {value}")
-            data_dict[component_name] = value
-        elif data_type == "d":
-            value = read_64d(open_file=open_file)
-            LOGGER.debug(f"component name: {component_name} | dtype: {data_type} | value: {value}")
-            data_dict[component_name] = value
-        elif data_type == "s":
-            value = read_null_terminated_string(open_file=open_file)
-            LOGGER.debug(f"component name: {component_name} | dtype: {data_type} | value: {value}")
-            data_dict[component_name] = value
-        elif data_type == "D":
-            array_size = read_u32i(open_file=open_file)
-            LOGGER.debug(f"component name: {component_name} | dtype: {data_type}")
-            LOGGER.debug(f"array size: {array_size}")
-            data = np.zeros(array_size)
-            for index in range(array_size):
-                data[index] = read_64d(open_file=open_file)
-            if "xres" in data_dict and "yres" in data_dict:
-                data = data.reshape((data_dict["xres"], data_dict["yres"]))
-            data_dict["data"] = data
-
-        return open_file.tell() - initial_byte_pos
-
-    @staticmethod
-    def _gwy_print_dict(gwy_file_dict: dict, pre_string: str) -> None:
-        """
-        Recursively print nested dictionary.
-
-        Can be used to find labels and values of objects / components in the `.gwy` file.
-
-        Parameters
-        ----------
-        gwy_file_dict : dict
-            Dictionary of the nested object / component structure of a `.gwy` file.
-        pre_string : str
-            Prefix to use when printing string.
-        """
-        for key, value in gwy_file_dict.items():
-            if isinstance(value, dict):
-                print(pre_string + f"OBJECT: {key}")
-                pre_string += "  "
-                LoadScans._gwy_print_dict(gwy_file_dict=value, pre_string=pre_string)
-                pre_string = pre_string[:-2]
-            else:
-                print(pre_string + f"component: {key} | value: {value}")
-
-    @staticmethod
-    def _gwy_print_dict_wrapper(gwy_file_dict: dict) -> None:
-        """
-        Print dictionaries.
-
-        This is a wrapper for the _gwy_print_dict() method.
-
-        Parameters
-        ----------
-        gwy_file_dict : dict
-            Dictionary of the nested object / component structure of a `.gwy` file.
-        """
-        pre_string = ""
-        LoadScans._gwy_print_dict(gwy_file_dict=gwy_file_dict, pre_string=pre_string)
-
-    @staticmethod
-    def _gwy_get_channels(gwy_file_structure: dict) -> dict:
-        """
-        Extract a list of channels and their corresponding dictionary key ids from the `.gwy` file dictionary.
-
-        Parameters
-        ----------
-        gwy_file_structure : dict
-            Dictionary of the nested object / component structure of a `.gwy` file. Where the keys are object names
-            and the values are dictionaries of the object's components.
-
-        Returns
-        -------
-        dict
-            Dictionary where the keys are the channel names and the values are the dictionary key ids.
-
-        Examples
-        --------
-        # Using a loaded dictionary generated from a `.gwy` file:
-        LoadScans._gwy_get_channels(gwy_file_structure=loaded_gwy_file_dictionary)
-        """
-        title_key_pattern = re.compile(r"\d+(?=/data/title)")
-        channel_ids = {}
-
-        for key, _ in gwy_file_structure.items():
-            match = re.search(title_key_pattern, key)
-            if match:
-                channel = gwy_file_structure[key]
-                channel_ids[channel] = match.group()
-
-        return channel_ids
-
     def load_gwy(self) -> tuple[npt.NDArray, float]:
         """
         Extract image and pixel to nm scaling from the Gwyddion .gwy file.
@@ -909,48 +758,10 @@ class LoadScans:
         """
         LOGGER.debug(f"Loading image from : {self.img_path}")
         try:
-            image_data_dict = {}
-            with Path.open(self.img_path, "rb") as open_file:  # pylint: disable=unspecified-encoding
-                # Read header
-                header = open_file.read(4)
-                LOGGER.debug(f"Gwy file header: {header}")
-
-                LoadScans._gwy_read_object(open_file, data_dict=image_data_dict)
-
-            # For development - uncomment to have an indentation based nested
-            # dictionary output showing the object - component structure and
-            # available keys:
-            # LoadScans._gwy_print_dict_wrapper(gwy_file_dict=image_data_dict)
-
-            channel_ids = LoadScans._gwy_get_channels(gwy_file_structure=image_data_dict)
-
-            if self.channel not in channel_ids:
-                raise KeyError(
-                    f"Channel {self.channel} not found in {self.img_path.suffix} channel list: {channel_ids}"
-                )
-
-            # Get the image data
-            image = image_data_dict[f"/{channel_ids[self.channel]}/data"]["data"]
-            units = image_data_dict[f"/{channel_ids[self.channel]}/data"]["si_unit_xy"]["unitstr"]
-            # currently only support equal pixel sizes in x and y
-            px_to_nm = image_data_dict[f"/{channel_ids[self.channel]}/data"]["xreal"] / image.shape[1]
-
-            # Convert image heights to nanometresQ
-            if units == "m":
-                image = image * 1e9
-                px_to_nm = px_to_nm * 1e9
-            else:
-                raise ValueError(
-                    f"Units '{units}' have not been added for .gwy files. Please add \
-                    an SI to nanometre conversion factor for these units in _gwy_read_component in \
-                    io.py."
-                )
-
+            return gwy.load_gwy(file_path=self.img_path, channel=self.channel)
         except FileNotFoundError:
-            LOGGER.error(f"[{self.filename}] File not found : {self.img_path}")
+            LOGGER.error(f"File not found : {self.img_path}")
             raise
-
-        return (image, px_to_nm)
 
     def get_data(self) -> None:
         """Extract image, filepath and pixel to nm scaling value, and append these to the img_dic object."""
