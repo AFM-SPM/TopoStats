d00054c33495ab5967e4bc64d28fb0706e91db9e
Sylvia Whittle <86117496+SylviaWhittle@users.noreply.github.com>
2025-03-03 16:04:28 +0000

    From refs/pull/1022/merge a2baedffa91ae94a2df90bd1f63de8f4a22dc3e8

---
+++ v2.1.1/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2.3.2.dev212+g6d14c33f',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
