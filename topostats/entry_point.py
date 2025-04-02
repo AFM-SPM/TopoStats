7a1723ac3d97c3e334fecee4845b28acc716dae0
Max Gamill <mcgamill1@sheffield.ac.uk>
2025-04-02 15:32:08 +0100

    add to entry point

--- topostats/entry_point.py
+++ topostats/entry_point.py
@@ -373,6 +373,13 @@ def create_parser() -> arg.ArgumentParser:
         required=False,
         help="Extract height profiles along maximum feret of molecules",
     )
+    process_parser.add_argument(
+        "--grainstats-class-names",
+        dest="grainstats_class_names",
+        type=list,
+        required=False,
+        help="Names of the objects found by the segmentation methods (objects are linked to the `class_number` output in the grainstats csv",
+    )
 
     # Disordered Tracing
     process_parser.add_argument(
