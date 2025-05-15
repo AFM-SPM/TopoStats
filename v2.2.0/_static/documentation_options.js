03041650eccf347196ad0d15214c3f2a6dbcb06c
Neil Shephard <n.shephard@sheffield.ac.uk>
2025-05-15 14:18:03 +0000

    From refs/pull/1157/merge 4027e6862523bb56757734b0878ea0c7cd7c2c66

---
+++ v2.2.0/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2.3.2.dev444+g825f0e8f',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
