d0954aace3a1e370002808562eb084ef9319e71c
SylviaWhittle <sylviwhittle@gmail.com>
2025-05-30 10:59:38 +0100

    Add tests for re_crop_grain_image_and_mask_to_set_size_nm

--- tests/test_array_manipulation.py
+++ tests/test_array_manipulation.py
@@ -64,6 +64,228 @@ def test_pad_bounding_box_dynamically_at_limits(
     padding: int,
     expected_bbox: tuple[int, int, int, int],
 ) -> None:
-    """Test the pad_bounding_box_cutting_off_at_image_bounds method with dynamic limits."""
+    """Test the pad_bounding_box_dynamically_at_limits method."""
     result = pad_bounding_box_dynamically_at_limits(bbox=bbox, limits=limits, padding=padding)
     assert result == expected_bbox
+
+
+@pytest.mark.parametrize(
+    (
+        "grain_bbox",
+        "pixel_to_nm_scaling",
+        "full_image",
+        "full_mask_tensor",
+        "target_size_nm",
+        "expected_grain_crop",
+        "expected_grain_tensor",
+    ),
+    [
+        pytest.param(
+            (0, 2, 5, 6),
+            1.0,
+            np.array(
+                [
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 2, 2, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 2, 2, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 2, 2, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                ]
+            ).astype(np.float32),
+            np.stack(
+                [
+                    np.array(
+                        [
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                        ]
+                    ),
+                    np.array(
+                        [
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        ]
+                    ),
+                ],
+                axis=-1,
+            ).astype(np.bool_),
+            8.0,
+            np.array(
+                [
+                    [1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 2, 2, 1, 1, 1],
+                    [1, 1, 1, 2, 2, 1, 1, 1],
+                    [1, 1, 1, 2, 2, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1],
+                ]
+            ),
+            np.stack(
+                [
+                    np.array(
+                        [
+                            [1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 0, 0, 1, 1, 1],
+                            [1, 1, 1, 0, 0, 1, 1, 1],
+                            [1, 1, 1, 0, 0, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1],
+                        ]
+                    ),
+                    np.array(
+                        [
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 1, 1, 0, 0, 0],
+                            [0, 0, 0, 1, 1, 0, 0, 0],
+                            [0, 0, 0, 1, 1, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                        ]
+                    ),
+                ],
+                axis=-1,
+            ),
+            id="grain crop 8nm, even centred, restricted at top",
+        ),
+        pytest.param(
+            (0, 2, 5, 6),
+            1.0,
+            np.array(
+                [
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 2, 2, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 2, 2, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 2, 2, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                ]
+            ).astype(np.float32),
+            np.stack(
+                [
+                    np.array(
+                        [
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                        ]
+                    ),
+                    np.array(
+                        [
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        ]
+                    ),
+                ],
+                axis=-1,
+            ).astype(np.bool_),
+            7.0,
+            np.array(
+                [
+                    [1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 2, 2, 1, 1, 1],
+                    [1, 1, 2, 2, 1, 1, 1],
+                    [1, 1, 2, 2, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1],
+                ]
+            ),
+            np.stack(
+                [
+                    np.array(
+                        [
+                            [1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 0, 0, 1, 1, 1],
+                            [1, 1, 0, 0, 1, 1, 1],
+                            [1, 1, 0, 0, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1, 1, 1],
+                        ]
+                    ),
+                    np.array(
+                        [
+                            [0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 1, 1, 0, 0, 0],
+                            [0, 0, 1, 1, 0, 0, 0],
+                            [0, 0, 1, 1, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0],
+                        ]
+                    ),
+                ],
+                axis=-1,
+            ),
+            id="grain crop 7nm, odd centred, restricted at top",
+        ),
+    ],
+)
+def test_re_crop_grain_image_and_mask_to_set_size_nm(
+    grain_bbox: tuple[int, int, int, int],
+    pixel_to_nm_scaling: float,
+    full_image: npt.NDArray[np.float32],
+    full_mask_tensor: npt.NDArray[np.bool_],
+    target_size_nm: float,
+    expected_grain_crop: npt.NDArray[np.float32],
+    expected_grain_tensor: npt.NDArray[np.bool_],
+) -> None:
+    """Test the re_crop_grain_image_and_mask_to_set_size_nm method."""
+    grain_crop, grain_tensor = re_crop_grain_image_and_mask_to_set_size_nm(
+        filename="test_image",
+        grain_number=1,
+        grain_bbox=grain_bbox,
+        pixel_to_nm_scaling=pixel_to_nm_scaling,
+        full_image=full_image,
+        full_mask_tensor=full_mask_tensor,
+        target_size_nm=target_size_nm,
+    )
+
+    np.testing.assert_array_equal(grain_crop, expected_grain_crop)
+    np.testing.assert_array_equal(grain_tensor, expected_grain_tensor)
