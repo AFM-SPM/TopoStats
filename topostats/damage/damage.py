0bd592b890ecd3fec7dd1d2cccdd1d99b2f69042
SylviaWhittle <sylviwhittle@gmail.com>
2025-07-01 10:24:13 +0100

    remove earlier attempts at defect location calculations

--- topostats/damage/damage.py
+++ topostats/damage/damage.py
@@ -5,59 +5,6 @@ import numpy as np
 import numpy.typing as npt
 
 
-def calculate_defects_and_gap_lengths(
-    points_distance_to_previous_nm: npt.NDArray[np.float64],
-    defects_bool: npt.NDArray[np.bool_],
-) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
-    """Calculate the lengths of defects"""
-    defect_lengths = []
-    gap_lengths = []
-
-    current_gap_length = 0.0
-    current_defect_length = 0.0
-
-    for index, point in enumerate(defects_bool):
-
-        if point:
-            current_defect_length += points_distance_to_previous_nm[index]
-            if current_gap_length > 0:
-                # End the current gap
-                gap_lengths.append(current_gap_length)
-                current_gap_length = 0.0
-        else:
-            if current_defect_length > 0:
-                defect_lengths.append(current_defect_length)
-                current_defect_length = 0.0
-            # Continue the gap
-            current_gap_length += points_distance_to_previous_nm[index]
-
-    # if the last defect is still open, check if it's connected to the start defect and combine if necessary
-    if current_defect_length > 0:
-        if defects_bool[0]:
-            # Check if there are any defects in the list, if not, then the whole thing is a defect
-            if len(defect_lengths) == 0:
-                defect_lengths.append(current_defect_length)
-            else:
-                # End is connected to start, so combine the lengths into the first defect
-                defect_lengths[0] += current_defect_length
-        else:
-            # End is not connected to start, so add as a new defect
-            defect_lengths.append(current_defect_length)
-    else:
-        # Check if there is a gap in the list, if not, then there is just one big gap
-        if len(gap_lengths) == 0:
-            gap_lengths.append(current_gap_length)
-        else:
-            # There is a gap already in the list, so we now check if this gap is connected to the first gap
-            # If the first point is also a gap, we can combine it with the last gap
-            if current_gap_length > 0 and not defects_bool[0]:
-                gap_lengths[0] += current_gap_length
-            else:
-                gap_lengths.append(current_gap_length)
-
-    return np.array(defect_lengths), np.array(gap_lengths)
-
-
 def get_defect_start_end_indexes(
     defects_bool: npt.NDArray[np.bool_],
 ) -> tuple[npt.NDArray[np.int_], npt.NDArray[np.int_]]:
@@ -370,6 +317,7 @@ def calculate_distance_of_region(
         end_half_distance = distance_to_previous_points_nm[end_index + 1] / 2
         return distance_to_end + distance_to_start + start_half_distance + end_half_distance
 
+
 def get_defects_and_gaps_from_bool_array(
     defects_bool: npt.NDArray[np.bool_],
     circular: bool,
@@ -391,6 +339,7 @@ def get_defects_and_gaps_from_bool_array(
     )
 
     return defect_gap_list
+
 
 def calculate_defect_and_gap_lengths(
     distance_to_previous_points_nm: npt.NDArray[np.float64],
