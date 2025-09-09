03f8702ff2c9e00a76d5b874019f3339ce55e7c3
llwiggins <91545985+llwiggins@users.noreply.github.com>
2025-09-09 10:54:09 +0000

    From refs/pull/1208/merge 0fed9c59c9334d032e846bec9868d0f309ea9fa3

---
+++ v2.2.0/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2.3.2.dev568+g88b3f8ac6',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
