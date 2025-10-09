b983d22ea65f504ba2fea539b48e5ee40e5f0e1d
tobyallwood <55159915+tobyallwood@users.noreply.github.com>
2025-10-09 17:09:43 +0000

    From refs/pull/1218/merge 6a76ebe7b690f9192bfd4a852d355bb74947aabe

---
+++ v2.3.1/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2.3.2.dev577+g449b0d4d7',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
