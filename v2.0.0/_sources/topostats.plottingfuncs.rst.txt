a9672fd2057cd8eda53a82bca14b90d257726f94
tobyallwood <55159915+tobyallwood@users.noreply.github.com>
2025-10-23 12:50:37 +0000

    From refs/pull/1226/merge 6cf0b664eda917aa22cb9dfdeb8263f7f02ebc33

---
+++ v2.0.0/_sources/topostats.plottingfuncs.rst.txt
@@ -0,0 +1,5 @@
+plottingfuncs
+=============
+
+.. automodule:: topostats.plottingfuncs
+   :members:
---
+++ v2.2.0/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2.3.2.dev586+ga764b9b50',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
