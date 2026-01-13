72d4d984d14456ef3d4b3d46bf40a69e3aa16bd6
Neil Shephard <n.shephard@sheffield.ac.uk>
2026-01-13 13:26:07 +0000

    fix(tests|statistics): update test for statistics.image_statistics()
    The function `statistics.image_statistics()` now returns a dictionary for each image that is processed, these are
    amalgamated into a data frame and then written to CSV.
    
    The test therefore needed updating to reflect this change.

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,70 +1,36 @@
 """Tests for image statistics."""
 
 import numpy as np
-import pandas as pd
 import pytest
 
+from topostats.io import dict_almost_equal
 from topostats.statistics import image_statistics, roughness_rms
 
 
 def test_image_statistics(image_random: np.ndarray) -> None:
     """Test the image_statistics function of statistics.py."""
-    # Construct the results dataframe that image_stats takes as an argument
-    results_data = {
-        "molecule_number": [0, 1, 2, 3, 4, 5],
-        "threshold": ["above", "above", "below", "above", "below", "above"],
-    }
-    results_df = pd.DataFrame(results_data)
-
     # Collect the output dataframe
     output_image_stats = image_statistics(
         image=image_random,
         filename="test_image_file",
-        results_df=results_df,
         pixel_to_nm_scaling=0.5,
+        n_grains=4,
     )
+    expected = {
+        "image": "test_image_file",
+        "image_size_x_m": 5.12e-07,
+        "image_size_y_m": 5.12e-07,
+        "image_area_m2": 2.62144e-13,
+        "image_size_x_px": 1024,
+        "image_size_y_px": 1024,
+        "image_area_px2": 1048576,
+        "grains": 4,
+        "grains_per_m2": 15258789062499.998,
+        "rms_roughness": np.float64(5.772928703606123e-10),
+    }
 
-    # Construct the expected dataframe
-    expected_columns = [
-        "image",
-        "image_size_x_m",
-        "image_size_y_m",
-        "image_area_m2",
-        "image_size_x_px",
-        "image_size_y_px",
-        "image_area_px2",
-        "grains_number_above",
-        "grains_per_m2_above",
-        "grains_number_below",
-        "grains_per_m2_below",
-        "rms_roughness",
-    ]
-    expected_data = [
-        [
-            "test_image_file",
-            5.12e-07,
-            5.12e-07,
-            2.62144e-13,
-            1024,
-            1024,
-            1048576,
-            4,
-            15258789062499.998,
-            2,
-            7629394531249.999,
-            5.772928703606123e-10,
-        ]
-    ]
-    expected_df = pd.DataFrame(expected_data, columns=expected_columns)
-    expected_df.set_index("image", inplace=True)
-
-    # This was the only way I could find to do it, as pandas' assert_frame_equal will
-    # take 5e-10 and 4e-10 to be equal.
-    expected_values = np.array(expected_df.values[0])
-    output_values = np.array(output_image_stats.values[0])
-    np.testing.assert_array_equal(output_values, expected_values)
-
-    pd.testing.assert_frame_equal(output_image_stats, expected_df)
+    assert list(output_image_stats.keys()) == list(expected.keys())
+    assert dict_almost_equal(output_image_stats, expected)
 
 
 @pytest.mark.parametrize(
