7bb0480f37841375c130cd2e05f131d15f95dab1
SylviaWhittle <sylviwhittle@gmail.com>
2026-05-19 13:51:54 +0100

    Remove debug tooling but keep in history for future reference

--- topostats/damage/damage_processing.py
+++ topostats/damage/damage_processing.py
@@ -845,12 +845,6 @@ def calculate_turn_in_distance_for_trace_deg(
     turn_in_distances_deg = np.zeros(len(trace_coords_nm))
     distances_to_previous_points_nm = distances_nm(trace_coords_nm, circular)
 
-    from pathlib import Path
-    import matplotlib.pyplot as plt
-
-    debug_plot_dir = Path("/Users/sylvi/topo_data/temp")
-    plot_interval = 20
-
     # for each point, construct a symmetric window around it with the given window length
     for index in range(len(trace_coords_nm)):
         try:
@@ -919,85 +913,6 @@ def calculate_turn_in_distance_for_trace_deg(
         turn_rad = angle_diff_signed(v1=left_vector, v2=right_vector)
         turn_deg = np.degrees(turn_rad)
         turn_in_distances_deg[index] = turn_deg
-
-        plot = index % plot_interval == 0
-        if plot:
-            unique_id = np.sum(trace_coords_nm)
-            debug_fig, debug_ax = plt.subplots(figsize=(10, 10))
-            max_x_coord = np.max(trace_coords_nm[:, 0])
-            max_y_coord = np.max(trace_coords_nm[:, 1])
-            max_coord = max(max_x_coord, max_y_coord)
-            debug_ax.set_xlim(0, float(max_coord) * 1.1)
-            debug_ax.set_ylim(0, float(max_coord) * 1.1)
-            debug_ax.plot(trace_coords_nm[:, 0], trace_coords_nm[:, 1], label="trace")
-
-            # plot the window
-            debug_ax.scatter(
-                trace_coords_nm[left_index, 0],
-                trace_coords_nm[left_index, 1],
-                color="orange",
-                s=20,
-            )
-            debug_ax.scatter(
-                trace_coords_nm[right_index, 0],
-                trace_coords_nm[right_index, 1],
-                color="blue",
-                s=20,
-            )
-
-            for left_vector in left_vectors:
-                debug_ax.arrow(
-                    trace_coords_nm[left_index, 0],
-                    trace_coords_nm[left_index, 1],
-                    left_vector[0],
-                    left_vector[1],
-                    color="orange",
-                    head_width=2,
-                    head_length=5,
-                )
-            for right_vector in right_vectors:
-                debug_ax.arrow(
-                    trace_coords_nm[right_index, 0],
-                    trace_coords_nm[right_index, 1],
-                    right_vector[0],
-                    right_vector[1],
-                    color="blue",
-                    head_width=2,
-                    head_length=5,
-                )
-            debug_ax.arrow(
-                trace_coords_nm[left_index, 0],
-                trace_coords_nm[left_index, 1],
-                left_vector[0] * 4,
-                left_vector[1] * 4,
-                color="red",
-                head_width=5,
-                head_length=10,
-            )
-
-            debug_ax.arrow(
-                trace_coords_nm[right_index, 0],
-                trace_coords_nm[right_index, 1],
-                right_vector[0] * 4,
-                right_vector[1] * 4,
-                color="purple",
-                head_width=5,
-                head_length=10,
-            )
-            debug_ax.text(
-                trace_coords_nm[index, 0],
-                trace_coords_nm[index, 1],
-                f"{turn_rad:.2f} rad",
-                color="green",
-            )
-            debug_ax.text(
-                trace_coords_nm[index, 0],
-                trace_coords_nm[index, 1] - 5,
-                f"{turn_deg:.2f} deg",
-                color="green",
-            )
-            plt.savefig(debug_plot_dir / f"{unique_id}_{index}_turn_in_distance_debug_plot.png")
-            plt.close(debug_fig)
 
     return turn_in_distances_deg
 
