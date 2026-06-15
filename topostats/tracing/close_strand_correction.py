389df1ecabaf007c7cef1d0fc6dcbbde77b7ebd8
SylviaWhittle <sylviwhittle@gmail.com>
2026-06-15 17:43:08 +0100

    [WIP] True crossing improvement - needs checking etc

--- topostats/tracing/close_strand_correction.py
+++ topostats/tracing/close_strand_correction.py
@@ -94,6 +94,58 @@ def get_point_along_branch(
     return current_coord, np.array(path_taken)
 
 
+def trace_skeleton_better_through_node(
+    node_coords: tuple[int, int],
+    peak_coords: tuple[int, int],
+    image: npt.NDArray[np.float64],
+    skeleton: npt.NDArray[np.bool_],
+) -> None:
+    euclidean_distance_to_peak_px = np.linalg.norm((peak_coords[0] - node_coords[0]), (peak_coords[1] - node_coords[1]))
+    number_of_interpolation_steps = np.max(int(euclidean_distance_to_peak_px * 2.0), 5)
+
+    to_fill_points: list[tuple[int, int]] = []
+    for index in range(number_of_interpolation_steps):
+        interp_factor = index / (number_of_interpolation_steps - 1)  # since working in range 0 - num -1.
+        # find the point along the line proportional to the interpolation factor
+        interp_point = (1 - interp_factor) * node_coords + (interp_factor) * peak_coords
+
+        if index == 0:
+            to_fill_points.append(node_coords)
+        if index == number_of_interpolation_steps - 1:
+            to_fill_points.append(peak_coords)
+
+        vector_of_interpolated_point_peak_line = (peak_coords[0] - node_coords[0], peak_coords[1] - node_coords[1])
+        vector_of_interpolated_point_peak_line_length_px = np.linalg.norm(vector_of_interpolated_point_peak_line)
+        normal_of_interpolated_point_peak_line = (
+            np.array([-vector_of_interpolated_point_peak_line[1], vector_of_interpolated_point_peak_line[0]])
+            / vector_of_interpolated_point_peak_line_length_px
+        )
+
+        # search for the best (highest) point along the normal, tapering the search distance as we get closer to the
+        # peak point to prevent sharp turns
+        best_point: tuple[int, int] = (int(interp_point[0]), int(interp_point[1]))
+        maximum_height = -np.inf
+        search_offsets_px = [-1.5, -1.0, -0.5, 0.0, 0.5, 1.0, 1.5]
+        for offset in search_offsets_px:
+            # taper the offset as proximity to the peak increases.
+            tapered_offset = offset * (1 - interp_factor)
+            test_point = interp_point + (normal_of_interpolated_point_peak_line * tapered_offset)
+            test_point = (int(test_point[0]), int(test_point[1]))
+
+            # check for image bounds
+            if 0 <= test_point[0] < image.shape[1] and 0 <= test_point[1] < image.shape[0]:
+                # TODO: check the indexing here
+                height = image[test_point[1], test_point[0]]
+                if height > maximum_height:
+                    maximum_height = height
+                    best_point = test_point
+        to_fill_points.append(best_point)
+
+    # Fill the skeleton
+    for to_fill_point in to_fill_points:
+        skeleton[to_fill_point[1], to_fill_point[0]] = True
+
+
 def correct_close_strands_image(  # noqa: C901
     topostats_object: TopoStats,
     true_peak_threshold_median_multiplier: float,
@@ -142,6 +194,7 @@ def correct_close_strands_image(  # noqa: C901
             node_pixel_coords = np.argwhere(mask_node == 1)
             node_pixel_heights = grain_image[node_pixel_coords[:, 0], node_pixel_coords[:, 1]]
             max_node_pixel_height = np.max(node_pixel_heights)
+            max_node_pixel_coords = node_pixel_coords[np.argmax(node_pixel_heights)]
             if max_node_pixel_height > true_peak_threshold:
                 # this is a true crossing (probably)
                 crossing_data.append(
@@ -155,7 +208,105 @@ def correct_close_strands_image(  # noqa: C901
                     }
                 )
                 # do true crossing adjustment
-                # TODO: true crossing adjustments
+
+                # Find node coordinates and clear the paths
+                mask_dilated_node = binary_dilation(mask_node, footprint=np.ones((3, 3)))
+                mask_dilated_node_branch_intersections = mask_dilated_node * mask_branches
+                branch_starts = np.argwhere(mask_dilated_node_branch_intersections == 1)
+                if branch_starts.shape[0] != 4:
+                    # to fix a true crossing, we need 4 branches - skip this node if not the case.
+                    continue
+
+                points: list[tuple[int, int]] = []
+                paths = []
+                for branch_start in branch_starts:
+                    point, path = get_point_along_branch(
+                        skeleton=mask_branches,
+                        start_coord=branch_start,
+                        distance_px=branch_explore_distance_nm / pixel_to_nm_scaling,
+                    )
+                    points.append((point[0], point[1]))
+                    paths.append(path)
+                    proposed_result_corrected_skeleton[path[:, 1], path[:, 0]] = False
+
+                # Improve the crossing
+
+                # list the possible combinations of pairs of points to connect
+                possible_combinations_indexes = [
+                    ((0, 1), (2, 3)),
+                    ((0, 2), (1, 3)),
+                    ((0, 3), (1, 2)),
+                ]
+
+                # get the unit vectors from each node towards the peak pixel coord
+                unit_vectors_to_peak_pixel = []
+                for point in points:
+                    vector = max_node_pixel_coords - point
+                    unit_vector = vector / np.linalg.norm(vector)
+                    unit_vectors_to_peak_pixel.append(unit_vector)
+
+                # find the combination of pairs of nodes that have the smallest combined dot product of their vectors
+                # to the peak. ie - finding the combination where the highest point is closest to the line between the
+                # two points
+                best_combination_pair = None
+                min_dot_product_sum = np.inf
+                for combination_indexes in possible_combinations_indexes:
+                    pair_0_indexes, pair_1_indexes = combination_indexes
+                    combined_to_peak_dot_product = np.dot(
+                        unit_vectors_to_peak_pixel[pair_0_indexes[0]], unit_vectors_to_peak_pixel[pair_0_indexes[1]]
+                    ) + np.dot(
+                        unit_vectors_to_peak_pixel[pair_1_indexes[0]], unit_vectors_to_peak_pixel[pair_1_indexes[1]]
+                    )
+                    if combined_to_peak_dot_product < min_dot_product_sum:
+                        best_combination_pair = combination_indexes
+
+                assert best_combination_pair is not None
+                strand_1_start, strand_1_end = (
+                    points[best_combination_pair[0][0]],
+                    points[best_combination_pair[0][1]],
+                )
+                strand_2_start, strand_2_end = (
+                    points[best_combination_pair[1][0]],
+                    points[best_combination_pair[1][1]],
+                )
+
+                # trace a more apt line through the node for the skeleton
+                trace_skeleton_better_through_node(
+                    node_coords=strand_1_start,
+                    peak_coords=max_node_pixel_coords,
+                    image=grain_image,
+                    skeleton=proposed_result_corrected_skeleton,
+                )
+                trace_skeleton_better_through_node(
+                    node_coords=strand_1_end,
+                    peak_coords=max_node_pixel_coords,
+                    image=grain_image,
+                    skeleton=proposed_result_corrected_skeleton,
+                )
+                trace_skeleton_better_through_node(
+                    node_coords=strand_2_start,
+                    peak_coords=max_node_pixel_coords,
+                    image=grain_image,
+                    skeleton=proposed_result_corrected_skeleton,
+                )
+                trace_skeleton_better_through_node(
+                    node_coords=strand_2_end,
+                    peak_coords=max_node_pixel_coords,
+                    image=grain_image,
+                    skeleton=proposed_result_corrected_skeleton,
+                )
+
+                # Sanity check for the node before applying the correction
+                # if the proposed correction now has disconnected components, skip this correction
+                if np.max(label(proposed_result_corrected_skeleton)) != 1:
+                    LOGGER.info(
+                        f"True crossing improvement :  grain [{grain_index}] node [{node_index}]: Proposed"
+                        f"improvement results in disconnected components. Skipping."
+                    )
+                    continue
+
+                result_corrected_skeleton = proposed_result_corrected_skeleton
+
             else:
                 # this is a false crossing (probably)
                 crossing_data.append(
@@ -195,7 +346,7 @@ def correct_close_strands_image(  # noqa: C901
                 # Also remove the node coords from the result skeleton
                 proposed_result_corrected_skeleton[mask_node == 1] = False
 
-                possible_combinations = [
+                possible_combinations_indexes = [
                     ((point_a, point_b), (point_c, point_d)),
                     ((point_a, point_c), (point_b, point_d)),
                     ((point_a, point_d), (point_b, point_c)),
@@ -204,8 +355,8 @@ def correct_close_strands_image(  # noqa: C901
                     tuple[tuple[np.ndarray, np.ndarray, np.ndarray], tuple[np.ndarray, np.ndarray, np.ndarray]] | None
                 ) = None
                 highest_lowest_height_along_paths = -np.inf
-                for combination in possible_combinations:
-                    pair_1, pair_2 = combination
+                for combination_indexes in possible_combinations_indexes:
+                    pair_1, pair_2 = combination_indexes
                     # get the costs for pathfinding between the pairs
                     start_1 = pair_1[0]
                     end_1 = pair_1[1]
